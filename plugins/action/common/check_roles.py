4568b0163f2aea12477d241199facfe7ab9ca55f
mwiebe <mwiebe@cisco.com>
2024-03-26 21:48:57 -0400

    host specific defaults and diff model changes

---
+++ plugins/action/common/check_roles.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, division, print_function
+
+
+__metaclass__ = type
+
+from ansible import constants as C
+from ansible.utils.display import Display
+from ansible.plugins.action import ActionBase
+
+from ..helper_functions import do_something
+
+from pprint import pprint
+
+display = Display()
+
+class ActionModule(ActionBase):
+
+    def run(self, tmp=None, task_vars=None):
+        # self._supports_async = True
+        results = super(ActionModule, self).run(tmp, task_vars)
+        results['save_previous'] = False
+
+        roles = self._task.args['role_list']
+        for role in ['cisco.nac_dc_vxlan.create', 'cisco.nac_dc_vxlan.remove']:
+            if role in roles:
+                results['save_previous'] = True
+
+        return results
