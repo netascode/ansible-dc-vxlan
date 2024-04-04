f4edf537d602e377fa241cee071d750269d8fa98
Mike Wiebe <mwiebe@cisco.com>
2024-04-03 21:25:07 -0400

    Inventory workflow updates (#38)
    * Inventory workflow updates
    
    * Revert change to template
    
    * Few more fixes
    
    * More workflow fixes
    
    * refactor checks for nonetype
    
    * refactor to handle nonetype
    
    ---------
    
    Co-authored-by: Matt Tarkington <mtarking@cisco.com>

--- plugins/action/common/prepare_plugins/104_prep_fabric_overlay_services.py
+++ plugins/action/common/prepare_plugins/104_prep_fabric_overlay_services.py
@@ -1,36 +1,43 @@
 class PreparePlugin:
     def __init__(self, **kwargs):
         self.kwargs = kwargs
-        self.keys = ['fabric', 'overlay_services', 'vrfs', 'networks']
+        self.keys = ['fabric', 'overlay_services', 'vrfs', 'vrf_attach_groups', 'networks', 'network_attach_groups']
 
     def prepare(self):
         model_data = self.kwargs['results']['model_extended']
 
         # Handle VRFs/Networks Under Overlay Services.  Need to create an empty list
         # if vrfs/networks or vrf/networks key is not present in the service model data
-        if model_data.get('fabric').get('overlay_services') is None:
-            model_data['fabric']['overlay_services'] = {'vrfs': []}
-            model_data['fabric']['overlay_services'] = {'networks': []}
-        if model_data.get('fabric').get('overlay_services').get('vrfs') is None:
-            model_data['fabric']['overlay_services']['vrfs'] = []
-        if model_data.get('fabric').get('overlay_services').get('networks') is None:
-            model_data['fabric']['overlay_services']['networks'] = []
+        if not model_data.get(self.keys[0], None):
+            if not model_data.get(self.keys[0]).get(self.keys[1], None):
+                model_data[self.keys[0]][self.keys[1]] = {self.keys[2]: []}
+                model_data[self.keys[0]][self.keys[1]] = {self.keys[4]: []}
+            if not model_data.get(self.keys[0]).get(self.keys[1]).get(self.keys[2], None):
+                model_data[self.keys[0]][self.keys[1]][self.keys[2]] = []
+            if not model_data.get(self.keys[0]).get(self.keys[1]).get(self.keys[4], None):
+                model_data[self.keys[0]][self.keys[1]][self.keys[4]] = []
 
         # Rebuild sm_data['fabric']['overlay_services']['vrf_attach_groups'] into
         # a structure that is easier to use.
-        model_data['fabric']['overlay_services']['vrf_attach_groups_dict'] = {}
-        for grp in model_data['fabric']['overlay_services']['vrf_attach_groups']:
-            model_data['fabric']['overlay_services']['vrf_attach_groups_dict'][grp['name']] = []
-            for switch in grp['switches']:
-                model_data['fabric']['overlay_services']['vrf_attach_groups_dict'][grp['name']].append(switch)
+        if model_data.get(self.keys[0], None):
+            if model_data.get(self.keys[0]).get(self.keys[1], None):
+                if model_data.get(self.keys[0]).get(self.keys[1]).get('vrf_attach_groups_dict', None):
+                    model_data[self.keys[0]][self.keys[1]]['vrf_attach_groups_dict'] = {}
+                    for grp in model_data[self.keys[0]][self.keys[1]][self.keys[3]]:
+                        model_data[self.keys[0]][self.keys[1]]['vrf_attach_groups_dict'][grp['name']] = []
+                        for switch in grp['switches']:
+                            model_data[self.keys[0]][self.keys[1]]['vrf_attach_groups_dict'][grp['name']].append(switch)
 
         # Rebuild sm_data['fabric']['overlay_services']['network_attach_groups'] into
         # a structure that is easier to use.
-        model_data['fabric']['overlay_services']['network_attach_groups_dict'] = {}
-        for grp in model_data['fabric']['overlay_services']['network_attach_groups']:
-            model_data['fabric']['overlay_services']['network_attach_groups_dict'][grp['name']] = []
-            for switch in grp['switches']:
-                model_data['fabric']['overlay_services']['network_attach_groups_dict'][grp['name']].append(switch)
+        if model_data.get(self.keys[0], None):
+            if model_data.get(self.keys[0]).get(self.keys[1], None):
+                if model_data.get(self.keys[0]).get(self.keys[1]).get('network_attach_groups_dict', None):
+                    model_data[self.keys[0]][self.keys[1]]['network_attach_groups_dict'] = {}
+                    for grp in model_data[self.keys[0]][self.keys[1]][self.keys[5]]:
+                        model_data[self.keys[0]][self.keys[1]]['network_attach_groups_dict'][grp['name']] = []
+                        for switch in grp['switches']:
+                            model_data[self.keys[0]][self.keys[1]]['network_attach_groups_dict'][grp['name']].append(switch)
 
         self.kwargs['results']['model_extended'] = model_data
         return self.kwargs['results']
