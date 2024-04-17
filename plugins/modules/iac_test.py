172e1a7ae0902b072914efe472f188500d359cc7
Matt Tarkington <mtarking@cisco.com>
2024-04-16 21:01:24 -0400

    update module with dir checks

--- plugins/modules/iac_test.py
+++ plugins/modules/iac_test.py
@@ -11,6 +11,9 @@ from ansible.module_utils.basic import AnsibleModule
 import iac_test.robot_writer
 import iac_test.pabot
 
+import sys
+from io import StringIO
+
 
 def run_module():
     module_args = dict(
@@ -41,31 +44,63 @@ def run_module():
     render_only = module.params["render_only"]
     dry_run = module.params["dry_run"]
 
-    if output and not os.path.exists(output):
-        module.fail_json(
-            msg=f"The provided directory {output} does not appear to exist. Is it a directory?"
-        )
+    paths = data
+    paths.append(templates)
+    if filters:
+        paths.append(filters)
+    if tests:
+        paths.append(tests)
+
+    for path in paths:
+        if not os.path.exists(path):
+            module.fail_json(msg=f"The provided directory {path} does not appear to exist!")
+
+    for path in paths:
+        if not os.listdir(path):
+            module.fail_json(msg=f"The provided directory {path} exists but appears to be empty!")
+
+    #### Option #1 ####
+    #### iac-test class object & methods ####
 
     # writer = iac_test.robot_writer.RobotWriter(data, filters, tests, include, exclude)
     # writer.write(templates, output)
-    # # iac_test.pabot.run_pabot(output, include, exclude, dry_run)
     # if not render_only:
-    #     iac_test.pabot.run_pabot(output, include, exclude, dry_run)
+    #     iac_test.pabot.run_pabot(outpiac_testut, include, exclude, dry_run)
+
+    #### Option #2 ####
+    #### iac-test cmd line wrapper ####
 
     options = []
 
-    if data:
+    for item in data:
         options.append("--data")
-        options.append(data[0])
-    if templates:
-        options.append("--templates")
-        options.append(templates)
-    if output:
-        options.append("--output")
-        options.append(output)
+        options.append(item)
+    options.append("--templates")
+    options.append(templates)
+    if filters:
+        options.append("--filters")
+        options.append(filters)
+    if tests:
+        options.append("--tests")
+        options.append(tests)
+    options.append("--output")
+    options.append(output)
+    if include:
+        for item in include:
+            options.append("--include")
+            options.append(item)
+    if exclude:
+        for item in exclude:
+            options.append("--exclude")
+            options.append(item)
+    if render_only:
+        options.append("--render-only")
+    if dry_run:
+        options.append("--dry-run")
 
     command = ["iac-test"]
     command.extend(options)
+    result["command"] = command
 
     rc, _, _ = module.run_command(command)
 
