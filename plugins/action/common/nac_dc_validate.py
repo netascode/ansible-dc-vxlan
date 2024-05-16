448ea89a7a5630314968f5815592027810067eff
Matt Tarkington <mtarking@cisco.com>
2024-05-16 10:42:12 -0400

    updates to allow splitting of required vs enhanced rules to work

--- plugins/action/common/nac_dc_validate.py
+++ plugins/action/common/nac_dc_validate.py
@@ -24,6 +24,8 @@ class ActionModule(ActionBase):
         schema = self._task.args.get('schema')
         rules = self._task.args.get('rules')
         mdata = self._task.args.get('mdata')
+        required_rules = f"{rules}/required_rules"
+        enhanced_rules = f"{rules}/enhanced_rules"
 
         # Generate a warning if the Schema and Rules are not provided
         if schema and not os.path.exists(schema):
@@ -33,6 +35,13 @@ class ActionModule(ActionBase):
         # The rules directory is considered empty if it is an empty dir or only contains the .gitkeep file
         if os.path.exists(rules) and (not os.listdir(rules) or (len(os.listdir(rules)) == 1 and '.gitkeep' in os.listdir(rules))):
             display.warning("The rules directory ({0}) exists but is empty! ".format(rules))
+        if not os.path.exists(required_rules):
+            display.warning("The required rules directory ({0}) does not appear to exist! ".format(required_rules))
+        # The required rules directory is considered empty if it is an empty dir or only contains the .gitkeep file
+        if os.path.exists(required_rules) and (not os.listdir(required_rules) or (len(os.listdir(required_rules)) == 1 and '.gitkeep' in os.listdir(required_rules))):
+            display.warning("The required rules directory ({0}) exists but is empty! ".format(required_rules))
+        if not os.path.exists(enhanced_rules):
+            display.warning("The enhanced rules directory ({0}) does not appear to exist! ".format(enhanced_rules))
 
         # Verify That Data Sources Exists
         if mdata and not os.path.exists(mdata):
@@ -47,16 +56,23 @@ class ActionModule(ActionBase):
         if schema is None:
             schema = ""
         if rules is None:
-            rules = ""
+            required_rules = ""
+            enhanced_rules = ""
 
-        validator = iac_validate.validator.Validator(schema, rules)
+        validator = iac_validate.validator.Validator(schema, required_rules)
         if schema:
             validator.validate_syntax([mdata])
-        if rules:
+        if required_rules:
             validator.validate_semantics([mdata])
+        # import epdb; epdb.set_trace()
+        if (enhanced_rules and os.path.exists(enhanced_rules) and (not os.listdir(enhanced_rules) or (len(os.listdir(enhanced_rules)) > 1 and '.gitkeep' in os.listdir(enhanced_rules)))):
+            enhanced_validator = iac_validate.validator.Validator(schema, enhanced_rules)
+            enhanced_validator.validate_semantics([mdata])
 
         msg = ""
         for error in validator.errors:
+            msg += error + "\n"
+        for error in enhanced_validator.errors:
             msg += error + "\n"
 
         if msg:
