3eb22a0a27d1af0232e676c6605621def3ea9a78
Matt Tarkington <mtarking@cisco.com>
2024-05-16 09:16:33 -0400

    stash get_creds update

--- plugins/action/common/get_credentials.py
+++ plugins/action/common/get_credentials.py
@@ -5,6 +5,9 @@ __metaclass__ = type
 
 from ansible.utils.display import Display
 from ansible.plugins.action import ActionBase
+import ansible.plugins.lookup.env
+from ansible.plugins.loader import lookup_loader
+
 import copy
 
 display = Display()
@@ -14,17 +17,24 @@ class ActionModule(ActionBase):
 
     def run(self, tmp=None, task_vars=None):
         # self._supports_async = True
+        # import epdb; epdb.set_trace()
+
+        # task_vars.get('ndfc_switch_username')
+
+        lookup = lookup_loader.get('ansible.builtin.env')
+        username = lookup.run(['NDFC_SWITCH_USER'], task_vars, default='admin')
+
         results = super(ActionModule, self).run(tmp, task_vars)
         results['failed'] = False
 
         inv_list = self._task.args['inv_list']
-        username = task_vars.get('ndfc_device_username')
-        password = task_vars.get('ndfc_device_password')
+        username = task_vars.get('ndfc_switch_username')
+        password = task_vars.get('ndfc_switch_password')
 
         # Fail if username and password are not set
         if username is None or password is None:
             results['failed'] = True
-            results['msg'] = "ndfc_device_username and ndfc_device_username must be set in group_vars or as environment variables!"
+            results['msg'] = "ndfc_switch_username and ndfc_switch_password must be set in group_vars or as environment variables!"
             # TODO: Add support for environemnt variables
             return results
 
