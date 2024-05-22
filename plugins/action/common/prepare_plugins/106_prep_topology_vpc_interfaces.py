92d27e4dfb5c227583b98f6705ec3dd476e1e2f0
mwiebe <mwiebe@cisco.com>
2024-05-22 17:59:59 -0400

    Add license info to source files

--- plugins/action/common/prepare_plugins/106_prep_topology_vpc_interfaces.py
+++ plugins/action/common/prepare_plugins/106_prep_topology_vpc_interfaces.py
@@ -1,3 +1,24 @@
+# Copyright (c) 2024 Cisco Systems, Inc. and its affiliates
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# SPDX-License-Identifier: MIT
+
 # Group vPC interfaces by vpc_peers, vpc_id and switch_name
 # This helps in identifying vPC interfaces for a given vpc_peer, vpc_id and switch_name
 # Reduces the need to loop through all interfaces to find vPC interfaces in Jinja2 templates
