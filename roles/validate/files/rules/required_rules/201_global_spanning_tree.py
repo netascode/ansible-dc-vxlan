a535b216f0089bd8b26a040837bfa452ed99bece
Matt Tarkington <mtarking@cisco.com>
2024-07-22 22:07:52 -0400

    add fabric stp

---
+++ roles/validate/files/rules/required_rules/201_global_spanning_tree.py
@@ -0,0 +1,26 @@
+class Rule:
+    id = "201"
+    description = "Verify a spanning tree protocol mutually exclusive parameters."
+    severity = "HIGH"
+
+    @classmethod
+    def match(cls, inventory):
+        results = []
+        if inventory.get("vxlan", None):
+            if inventory["vxlan"].get("global", None):
+                if inventory["vxlan"].get("global", None).get("spanning_tree", None):
+                    root_bridge_protocol = inventory["vxlan"]["global"]["spanning_tree"].get("root_bridge_protocol", None)
+                    vlan_range = inventory["vxlan"]["global"]["spanning_tree"].get("vlan_range", None)
+                    mst_instance_range = inventory["vxlan"]["global"]["spanning_tree"].get("vlan_range", None)
+
+                    if root_bridge_protocol == "rpvst+" and not mst_instance_range:
+                        results.append(
+                            f"vxlan.global.spanning_tree.vlan_range must be set when the spanning tree vxlan.global.spanning_tree.root_bridge_protocol is set to rpvst+."
+                        )
+
+                    if root_bridge_protocol == "mst" and not vlan_range:
+                        results.append(
+                            f"vxlan.global.spanning_tree.mst_instance_range can only be used when the spanning tree vxlan.global.spanning_tree.root_bridge_protocol is set to mst."
+                        )
+
+        return results
