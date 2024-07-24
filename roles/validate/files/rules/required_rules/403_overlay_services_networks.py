d81cf8cc12cbc94bbac350d8513ac8b9e4e477e8
Matt Tarkington <mtarking@cisco.com>
2024-07-24 13:07:20 -0400

    update network rules

--- roles/validate/files/rules/required_rules/403_overlay_services_networks.py
+++ roles/validate/files/rules/required_rules/403_overlay_services_networks.py
@@ -7,12 +7,18 @@ class Rule:
     def match(cls, inventory):
         results = []
         fabric_netflow_status = False
+        fabric_trm_status = False
         networks = []
 
         if inventory.get("vxlan", None):
             if inventory["vxlan"].get("global", None):
                 if inventory["vxlan"].get("global").get("netflow", None):
                     fabric_netflow_status = inventory["vxlan"]["global"]["netflow"].get("enable", False)
+
+        if inventory.get("vxlan", None):
+            if inventory["vxlan"].get("underlay", None):
+                if inventory["vxlan"].get("underlay").get("multicast", None):
+                    fabric_trm_status = inventory["vxlan"]["underlay"]["multicast"].get("trm_enable", False)
 
         if inventory.get("vxlan", None):
             if inventory["vxlan"].get("overlay_services", None):
@@ -26,6 +32,14 @@ class Rule:
                     results.append(
                         f"For vxlan.overlay_services.networks.{network['name']}.netflow_enable to be enabled, "
                         f"first vxlan.global.netflow.enable must be enabled (true)."
+                    )
+
+            current_network_trm_status = network.get("trm_enable", None)
+            if current_network_trm_status is not None:
+                if fabric_trm_status is False and current_network_trm_status is True:
+                    results.append(
+                        f"For vxlan.overlay_services.networks.{network['name']}.trm_enable to be enabled, "
+                        f"first vxlan.underlay.multicast.trm_enable must be enabled (true)."
                     )
 
         return results
