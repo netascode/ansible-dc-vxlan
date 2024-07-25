c80ed363ea2419fe85c3d7e9af06c8767477e0b0
Matt Tarkington <mtarking@cisco.com>
2024-07-24 21:48:19 -0400

    add additional trm rules to vrf

--- roles/validate/files/rules/required_rules/402_overlay_services_vrfs.py
+++ roles/validate/files/rules/required_rules/402_overlay_services_vrfs.py
@@ -51,4 +51,29 @@ class Rule:
                         f"first vxlan.underlay.multicast.trm_enable must be enabled (true)."
                     )
 
+                current_vrf_trm_no_rp = vrf.get("no_rp", None)
+                current_vrf_trm_rp_external = vrf.get("rp_external", None)
+                current_vrf_trm_rp_address = vrf.get("rp_address", None)
+                current_vrf_trm_rp_loopback_id = vrf.get("rp_loopback_id", None)
+                current_vrf_trm_overlay_multicast_group = vrf.get("overlay_multicast_group", None)
+                
+                if fabric_trm_status:
+                    if (current_vrf_trm_no_rp and current_vrf_trm_rp_external or
+                            current_vrf_trm_no_rp and current_vrf_trm_rp_address or
+                            current_vrf_trm_no_rp and current_vrf_trm_rp_loopback_id or
+                            current_vrf_trm_no_rp and current_vrf_trm_overlay_multicast_group):
+                        results.append(
+                            f"When vxlan.overlay_services.vrfs.{vrf['name']}.no_rp is enabled (true), "
+                            f"then vxlan.overlay_services.vrfs.{vrf['name']}.rp_external, "
+                            f"vxlan.overlay_services.vrfs.{vrf['name']}.rp_address, "
+                            f"vxlan.overlay_services.vrfs.{vrf['name']}.rp_loopback_id, "
+                            f"vxlan.overlay_services.vrfs.{vrf['name']}.overlay_multicast_group must be disabled (false)."
+                        )
+
+                    if current_vrf_trm_rp_external and current_vrf_trm_rp_loopback_id:
+                        results.append(
+                            f"When vxlan.overlay_services.vrfs.{vrf['name']}.rp_external is enabled (true), "
+                            f"then vxlan.overlay_services.vrfs.{vrf['name']}.rp_loopback_id must be disabled (false)."
+                        )
+
         return results
