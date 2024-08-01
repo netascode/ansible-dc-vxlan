7aa351e3d95b539b4231c18c56950d3b6cff66cd
Matt Tarkington <mtarking@cisco.com>
2024-08-01 09:19:29 -0400

    Fix VRFs Template Bug and Update Rules for VRFs and Networks (#165)
    * update network rules
    
    * fix bug in vrf template and update rules
    
    * fix lint errors
    
    * add additional trm rules to vrf
    
    * fix lint errors
    
    * add additional trm rules to vrf
    
    * refactor
    
    * updates to use break when rule match encountered

--- roles/validate/files/rules/required_rules/403_overlay_services_networks.py
+++ roles/validate/files/rules/required_rules/403_overlay_services_networks.py
@@ -7,12 +7,18 @@ class Rule:
     def match(cls, inventory):
         results = []
         fabric_netflow_status = False
+        fabric_trm_status = False
         networks = []
 
         if inventory.get("vxlan", None):
             if inventory["vxlan"].get("global", None):
                 if inventory["vxlan"].get("global").get("netflow", None):
                     fabric_netflow_status = inventory["vxlan"]["global"]["netflow"].get("enable", False)
+
+        if inventory.get("vxlan", None):
+            if inventory["vxlan"].get("underlay", None):
+                if inventory["vxlan"].get("underlay").get("multicast", None):
+                    fabric_trm_status = inventory["vxlan"]["underlay"]["multicast"].get("trm_enable", False)
 
         if inventory.get("vxlan", None):
             if inventory["vxlan"].get("overlay_services", None):
@@ -27,5 +33,25 @@ class Rule:
                         f"For vxlan.overlay_services.networks.{network['name']}.netflow_enable to be enabled, "
                         f"first vxlan.global.netflow.enable must be enabled (true)."
                     )
+                    break
+
+            if fabric_netflow_status and current_network_netflow_status:
+                current_network_netflow_monitor = network.get("vlan_netflow_monitor", None)
+                if current_network_netflow_monitor is None:
+                    results.append(
+                        f"When vxlan.overlay_services.networks.{network['name']}.netflow_enable is enabled, "
+                        f"then vxlan.overlay_services.networks.{network['name']}.vlan_netflow_monitor must be set "
+                        "to a valid value from vxlan.global.netflow."
+                    )
+                    break
+
+            current_network_trm_status = network.get("trm_enable", None)
+            if current_network_trm_status is not None:
+                if fabric_trm_status is False and current_network_trm_status is True:
+                    results.append(
+                        f"For vxlan.overlay_services.networks.{network['name']}.trm_enable to be enabled, "
+                        f"first vxlan.underlay.multicast.trm_enable must be enabled (true)."
+                    )
+                    break
 
         return results
