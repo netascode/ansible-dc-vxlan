af6466420439c4edb73361c36ea69300f7def2ec
Matt Tarkington <mtarking@cisco.com>
2024-08-25 18:03:58 -0400

    more updates for pr

--- plugins/action/common/prepare_plugins/prep_001_list_defaults.py
+++ plugins/action/common/prepare_plugins/prep_001_list_defaults.py
@@ -185,5 +185,20 @@ class PreparePlugin:
             self.model_data['vxlan']['policy'].update({'groups': []})
             self.model_data['vxlan']['policy'].update({'switches': []})
 
+        parent_keys = ['vxlan', 'policy', 'policies']
+        dm_check = data_model_key_check(self.model_data, parent_keys)
+        if 'policies' in dm_check['keys_not_found'] or 'policies' in dm_check['keys_no_data']:
+            self.model_data['vxlan']['policy']['policies'] = []
+
+        parent_keys = ['vxlan', 'policy', 'groups']
+        dm_check = data_model_key_check(self.model_data, parent_keys)
+        if 'groups' in dm_check['keys_not_found'] or 'groups' in dm_check['keys_no_data']:
+            self.model_data['vxlan']['policy']['groups'] = []
+
+        parent_keys = ['vxlan', 'policy', 'switches']
+        dm_check = data_model_key_check(self.model_data, parent_keys)
+        if 'switches' in dm_check['keys_not_found'] or 'switches' in dm_check['keys_no_data']:
+            self.model_data['vxlan']['policy']['switches'] = []
+
         self.kwargs['results']['model_extended'] = self.model_data
         return self.kwargs['results']
--- plugins/action/common/prepare_plugins/prep_107_policy.py
+++ plugins/action/common/prepare_plugins/prep_107_policy.py
@@ -28,5 +28,19 @@ class PreparePlugin:
     def prepare(self):
         model_data = self.kwargs['results']['model_extended']
 
+        # import epdb; epdb.st()
+
+        # Ensure that vrf_lite's switches are mappinged to their respective 
+        # management IP address from topology switches
+        topology_switches = model_data['vxlan']['topology']['switches']
+        policy_switches = model_data['vxlan']['policy']['switches']
+        for switch in policy_switches:
+            if any(sw['name'] == switch['name'] for sw in topology_switches):
+                found_switch = next((item for item in topology_switches if item["name"] == switch['name']))
+                if found_switch.get('management').get('management_ipv4_address'):
+                    switch['name'] = found_switch['management']['management_ipv4_address']
+                elif found_switch.get('management').get('management_ipv6_address'):
+                    switch['name'] = found_switch['management']['management_ipv6_address']
+
         self.kwargs['results']['model_extended'] = model_data
         return self.kwargs['results']
