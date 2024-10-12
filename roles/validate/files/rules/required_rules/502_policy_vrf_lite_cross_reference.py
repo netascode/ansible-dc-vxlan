ffed026cd09b52ee46f3e3fda05c92ddf8a44ca5
ccoueffe <ccoueffe@cisco.com>
2024-10-12 15:39:53 +0200

    clean PEP8 and rewrite with function

--- roles/validate/files/rules/required_rules/502_policy_vrf_lite_cross_reference.py
+++ roles/validate/files/rules/required_rules/502_policy_vrf_lite_cross_reference.py
@@ -1,13 +1,15 @@
 '''
 Validation Rules scenarios:
-1.	Switches in the VRF policies should be a subset of the switches in the switch topology.
-2.	BGP and OSPF in the same VRF Lite policy are not supported.
-3.	route_reflector_client AF IPv4 and IPv6 config for the BGP peers should not be allowed.
-4.  Check if OSPF Authentication is enabled, key is provided
+1.  Switches in the [insert vrf-lite switch model key structure here] should be defined in the vxlan.topology.switches
+2.  BGP and OSPF should be defined in two different policy.
+3.  route_reflector_client for AF IPv4 and IPv6 config should not be allowed.
+4.  Check if OSPF Authentication is enabled, key must be provided
 5.  Check AREA 0 is standard
-6.  Check static routes between Policies
+6.  Check static routes across Policies
 7.  Check ospf network type for Loopback
 '''
+
+
 class Rule:
     id = "502"
     description = "Verify VRF-Lites Cross Reference Between Policies, Groups, and Switches"
@@ -34,116 +36,175 @@ class Rule:
                 if inventory["vxlan"].get("overlay_extensions").get("vrf_lites", None):
                     vrf_lites = inventory["vxlan"]["overlay_extensions"]["vrf_lites"]
 
-        for policy in vrf_lites: # Check Global Policy
+        for policy in vrf_lites:   # Check Global Level
             if policy.get('name', None):
                 policies.append(policy['name'])
-                # Check if OSPF and BGP is enabled in the global policy
-                if "ospf" in policy and "bgp" in policy:
-                    results.append(
-                                f"In the policy: {policy['name']}, BGP and OSPF are configured " +
-                                  "in the same policy at the Global level." +
-                                  "Please use two different policies"
-                            )
-                    break
-                if policy.get('ospf'): # Check OSPF parameters
-                    if policy['ospf'].get('areas'):
-                        for area in policy['ospf']['areas']:
-                            if "id" in area and area.get('area_type'):
 
-                                # Check if AREA 0 is not standard
-                                if ((area['id'] == 0 or area['id'] == '0.0.0.0') and
-                                    area['area_type'] != 'standard') :
-                                    results.append(
-                                        f"In the policy: {policy['name']}, area backbone (0) " +
-                                        "is always standard not {area['area_type']}"
-                                    )
-                                    break
+                cls.check_global_ospf_and_bgp(results, policy)
+                cls.check_global_ospf_area(results, policy)
 
-                # Check per switches
-                if policy.get('switches'):
+                if policy.get('switches'):  # Check switch Level
                     for switch_policy in policy['switches']:
-                        ospf = False
-                        bgp = False
+                        cls.check_switch_level(results, switch_policy, policy,
+                                               inventory["vxlan"]['global']['bgp_asn'],
+                                               topology_switches, static_routes_compliance)
 
-                        # Check (bgp or bgp_peer) are enabled at the switch level
-                        if  "bgp" in switch_policy or "bgp_peers" in switch_policy:
-                            bgp = True
-                            for bgp_peer in switch_policy['bgp_peers']:
-                                # Check RR for af IPv4
-                                if bgp_peer.get('address_family_ipv4_unicast'):
-                                    if bgp_peer['address_family_ipv4_unicast'].get('route_reflector_client'):
-                                        if bgp_peer['address_family_ipv4_unicast']['route_reflector_client'] is True:
-                                            if bgp_peer['remote_as'] != inventory["vxlan"]['global']['bgp_asn']:
-                                                results.append(
-                                                    f"In the policy: {policy['name']}, BGP route-reflector is not allowed in AF IPv4."
-                                                )
-                                                break
+        return results
 
-                                # Check RR for af IPv6
-                                if bgp_peer.get('address_family_ipv6_unicast'):
-                                    if bgp_peer['address_family_ipv6_unicast'].get('route_reflector_client'):
-                                        if bgp_peer['address_family_ipv6_unicast']['route_reflector_client'] is True:
-                                            if bgp_peer['remote_as'] != inventory["vxlan"]['global']['bgp_asn']:
-                                                results.append(
-                                                    f"In the policy: {policy['name']}, BGP route-reflector is not allowed in AF IPv6."
-                                                )
-                                                break
+    @classmethod
+    def check_global_ospf_and_bgp(cls, results, policy):
+        '''
+        Check if OSPF and BGP is enabled in the global policy
+        '''
+        if "ospf" in policy and "bgp" in policy:
+            results.append(
+                f"In the policy: {policy['name']}, BGP and OSPF are configured " +
+                "in the same policy at the Global level. " +
+                "Please use two different policies"
+                )
 
-                        # Check OSPF is enabled at the switch -> interface level
-                        if switch_policy.get('interfaces', None):
-                            for interface in switch_policy['interfaces']:
-                                if interface.get('ospf'): # Check if OSPF is enabled
-                                    ospf = True
-
-                                    # Check if Authentification is required and Key is not provided.
-                                    if "auth_type" in interface['ospf'] and interface['ospf']['auth_type'] is not None:
-                                        if "auth_key" not in interface['ospf']:
-                                            results.append(
-                                            f"In the policy: {policy['name']}, auth_type is {interface['ospf']['auth_type']} but auth_key is missing"
-                                            )
-                                            break
-                                    # Check if Network type for Loopback interface is not Broadcast
-                                    if interface['ospf'].get('network_type'):
-                                        if interface['name'].startswith('Lo') and interface['ospf']['network_type'] == "broadcast":
-                                            results.append(
-                                                f"In the policy: {policy['name']}, network_type: broadcast is not supported with Loopback"
-                                            )
-
-                                    # Check if Adversise-subnet is used only for Loopback in ospf
-                                    if interface['ospf'].get('advertise_subnet'):
-                                        if not interface['name'].startswith('Lo') and interface['ospf']['advertise_subnet']:
-                                            results.append(
-                                                f"In the policy: {policy['name']} is configured on {interface['name']}, " +
-                                                "advertise_subnet: True is only supported with Loopback"
-                                            )
-
-
-                        if ospf is True and bgp is True:
+    @classmethod
+    def check_global_ospf_area(cls, results, policy):
+        '''
+        Check OSPF if backbone area is standard
+        '''
+        if policy.get('ospf'):  # Check OSPF parameters
+            if policy['ospf'].get('areas'):
+                for area in policy['ospf']['areas']:
+                    if "id" in area and area.get('area_type'):
+                        # Check if AREA 0 is not standard
+                        if ((area['id'] == 0 or area['id'] == '0.0.0.0')
+                                and area['area_type'] != 'standard'):
                             results.append(
-                                        f"In the policy: {policy['name']}, BGP and OSPF are configured in the same policy at the switch level." +
-                                        "Please use two different policies"
+                                f"In the policy: {policy['name']}, area backbone (0) " +
+                                "is always standard not {area['area_type']}"
                             )
-                            break
 
-                        # Check if switch is in the topology
-                        if list(filter(lambda topo: topo['name'] == switch_policy['name'],
-                                       topology_switches)):
-                            pass
-                        else:
+    @classmethod
+    def check_switch_level(cls, results, switch_policy, policy,
+                           infra_bgp_asn, topology_switches, static_routes_compliance):
+        '''
+        Check switch level
+        '''
+        ospf = False
+        bgp = False
+
+        # Check (bgp or bgp_peer) at the switch level
+        if "bgp" in switch_policy or "bgp_peers" in switch_policy:
+            bgp = True
+            if "bgp_peers" in switch_policy:
+                cls.check_switch_bgp_route_reflector(results,
+                                                     bgp_peers=switch_policy['bgp_peers'],
+                                                     fabric_asn=infra_bgp_asn,
+                                                     policy=policy['name'])
+
+        # Check OSPF at the switch level
+        if switch_policy.get('interfaces', None):
+            for interface in switch_policy['interfaces']:
+                if interface.get('ospf'):
+                    ospf = True
+                    cls.check_switch_ospf(results,
+                                          interface['ospf'],
+                                          interface['name'],
+                                          policy['name'])
+
+        # Check if OSPF and BGP is enabled
+        if ospf is True and bgp is True:
+            results.append(
+                f"In the policy: {policy['name']}, " +
+                "BGP and OSPF are configured in the same policy at the switch level. " +
+                "Please use two different policies"
+            )
+
+        # Check if switch exists in topology
+        cls.check_switch_in_topology(results,
+                                     switch_policy['name'],
+                                     topology_switches,
+                                     policy['name'])
+
+        # Check Static routes
+        if "static_routes" in switch_policy:
+            static_routes_compliance.append({"policy": policy['name'],
+                                             "vrf": policy['vrf'],
+                                             "switch": switch_policy['name'],
+                                             "routes": switch_policy['static_routes']})
+
+        cls.check_route_compliance_across_policies(results, static_routes_compliance)
+
+    @classmethod
+    def check_switch_in_topology(cls, results, switch, topology_switches, policy):
+        '''
+        Check if switch is in the topology
+        '''
+        if list(filter(lambda topo: topo['name'] == switch, topology_switches)):
+            pass
+        else:
+            results.append(
+                f"In the policy: {policy}, switch {switch} is not defined in the topology inventory"
+            )
+
+    @classmethod
+    def check_switch_ospf(cls, results, ospf, interface=None, policy=None):
+        '''
+        Check OSPF parameters
+        '''
+        # Check if key exists if authentication is enabled
+        if "auth_type" in ospf and ospf['auth_type'] is not None:
+            if "auth_key" not in ospf:
+                results.append(
+                    f"In the policy: {policy}, auth_type is {ospf['auth_type']} " +
+                    "but auth_key is missing"
+                )
+
+        # Check if Network type for Loopback interface is not Broadcast
+        if ospf.get('network_type'):
+            if interface.startswith('Lo') and ospf['network_type'] == "broadcast":
+                results.append(
+                    f"In the policy: {policy}, network_type: " +
+                    "broadcast is not supported with Loopback"
+                )
+
+        # Check if Adversise-subnet is used only for Loopback in ospf
+        if ospf.get('advertise_subnet'):
+            if not interface.startswith('Lo') and ospf['advertise_subnet']:
+                results.append(
+                    f"In the policy: {policy} is configured on {interface}, " +
+                    "advertise_subnet: True is only supported with Loopback"
+                )
+
+    @classmethod
+    def check_switch_bgp_route_reflector(cls, results, bgp_peers, fabric_asn, policy):
+        '''
+        Check if route-reflector is enabled in eBGP
+        '''
+        for bgp_peer in bgp_peers:
+            # Check RR for AF IPv4
+            if bgp_peer.get('address_family_ipv4_unicast'):
+                if bgp_peer['address_family_ipv4_unicast'].get('route_reflector_client'):
+                    if bgp_peer['address_family_ipv4_unicast']['route_reflector_client'] is True:
+                        if bgp_peer['remote_as'] != fabric_asn:
                             results.append(
-                                f"In the policy: {policy['name']}, switch {switch_policy['name']} is not defined in the topology inventory"
+                                f"In the policy: {policy}, " +
+                                "BGP route-reflector is not allowed in AF IPv4."
                             )
-                            break
 
-                        # Check Static routes
-                        if "static_routes" in switch_policy:
-                            static_routes_compliance.append({"policy": policy['name'],
-                                                             "vrf": policy['vrf'],
-                                                             "switch": switch_policy['name'],
-                                                             "routes": switch_policy['static_routes']})
+            # Check RR for AF IPv6
+            if bgp_peer.get('address_family_ipv6_unicast'):
+                if bgp_peer['address_family_ipv6_unicast'].get('route_reflector_client'):
+                    if bgp_peer['address_family_ipv6_unicast']['route_reflector_client'] is True:
+                        if bgp_peer['remote_as'] != fabric_asn:
+                            results.append(
+                                f"In the policy: {policy}, " +
+                                "BGP route-reflector is not allowed in AF IPv6."
+                            )
 
-        for route in static_routes_compliance:
-            for route2 in static_routes_compliance:
+    @classmethod
+    def check_route_compliance_across_policies(cls, results, routes):
+        '''
+        Check routes compliance across policies
+        '''
+        for route in routes:
+            for route2 in routes:
                 if route['vrf'] == route2['vrf']:
                     if route['switch'] == route2['switch']:
                         for nb_pref in range(len(route['routes'])):
@@ -161,34 +222,34 @@ class Rule:
                                                 base_nh = route['routes'][nb_pref]['next_hops'][nb_nh]['ip']
                                                 list_nh = route2['routes'][nb_pref]['next_hops']
                                                 # Check if next-hop are is present in the list
-                                                if list(filter(lambda nh: nh['ip'] == base_nh , list_nh)):
+                                                if list(filter(lambda nh: nh['ip'] == base_nh, list_nh)):
                                                     pass
                                                 else:
                                                     results.append(
-                                    f"In the policy {route['policy']} Next Hop are different. "
-                                    f"Local: {route['routes'][nb_pref]['next_hops']} "
-                                    f"- Remote: {route2['routes'][nb_pref]['next_hops']}"
+                                                        f"In the policy {route['policy']} Next Hop are different. "
+                                                        f"Local: {route['routes'][nb_pref]['next_hops']} "
+                                                        f"- Remote: {route2['routes'][nb_pref]['next_hops']}"
                                                     )
                                                     break
                                             else:
                                                 results.append(
-                                    f"In the policy {route['policy']} Nb Next Hop are different. "
-                                    f"Local: {route['routes'][nb_pref]['next_hops']} "
-                                    f"- Remote: {route2['routes'][nb_pref]['next_hops']}"
+                                                    f"In the policy {route['policy']} Nb Next Hop are different. "
+                                                    f"Local: {route['routes'][nb_pref]['next_hops']} "
+                                                    f"- Remote: {route2['routes'][nb_pref]['next_hops']}"
                                                 )
                                                 break
                                     else:
                                         results.append(
-                                    f"In the policy {route['policy']} Route Tag are different. "
-                                    f"Local: {route['routes'][nb_pref]['route_tag']} "
-                                    f"- Remote: {route2['routes'][nb_pref]['route_tag']}"
+                                            f"In the policy {route['policy']} Route Tag are different. "
+                                            f"Local: {route['routes'][nb_pref]['route_tag']} "
+                                            f"- Remote: {route2['routes'][nb_pref]['route_tag']}"
                                         )
                                         break
                                 else:
                                     results.append(
-                                    f"In the policy {route['policy']} Prefixes are different. "
-                                    f"Local: {route['routes'][nb_pref]['prefix']} "
-                                    f"- Remote: {route2['routes'][nb_pref]['prefix']}"
+                                        f"In the policy {route['policy']} Prefixes are different. "
+                                        f"Local: {route['routes'][nb_pref]['prefix']} "
+                                        f"- Remote: {route2['routes'][nb_pref]['prefix']}"
                                     )
                                     break
                             else:
@@ -196,4 +257,3 @@ class Rule:
                                     f"In the policy {route['policy']} Nb prefixes are different. "
                                     f"Local: {route['routes']} - Remote: {route2['routes']}"
                                 )
-        return results
