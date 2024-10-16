9d1aa85912e89b2f0cc62c934eee8d42ffc1341c
Matt Thurston <mthursto@cisco.com>
2024-10-16 11:04:27 +0100

    many changes made for federated fabric overlay management

--- plugins/action/common/prepare_plugins/prep_103_topology_switches.py
+++ plugins/action/common/prepare_plugins/prep_103_topology_switches.py
@@ -28,28 +28,29 @@ class PreparePlugin:
     def prepare(self):
         model_data = self.kwargs['results']['model_extended']
         #  Loop over all the roles in vxlan.topology.switches.role
-        model_data['vxlan']['topology']['spine'] = {}
-        model_data['vxlan']['topology']['leaf'] = {}
-        model_data['vxlan']['topology']['border'] = {}
-        model_data['vxlan']['topology']['border_spine'] = {}
-        model_data['vxlan']['topology']['border_gateway'] = {}
-        model_data['vxlan']['topology']['border_gateway_spine'] = {}
-        model_data['vxlan']['topology']['super_spine'] = {}
-        model_data['vxlan']['topology']['border_super_spine'] = {}
-        model_data['vxlan']['topology']['border_gateway_super_spine'] = {}
-        model_data['vxlan']['topology']['tor'] = {}
-        sm_switches = model_data['vxlan']['topology']['switches']
-        for switch in sm_switches:
-            # Build list of switch IP's based on role keyed by switch name
-            name = switch.get('name')
-            role = switch.get('role')
-            model_data['vxlan']['topology'][role][name] = {}
-            v4_key = 'management_ipv4_address'
-            v6_key = 'management_ipv6_address'
-            v4ip = switch.get('management').get(v4_key)
-            v6ip = switch.get('management').get(v6_key)
-            model_data['vxlan']['topology'][role][name][v4_key] = v4ip
-            model_data['vxlan']['topology'][role][name][v6_key] = v6ip
+        if model_data['vxlan'].get('topology', None) is not None:
+            model_data['vxlan']['topology']['spine'] = {}
+            model_data['vxlan']['topology']['leaf'] = {}
+            model_data['vxlan']['topology']['border'] = {}
+            model_data['vxlan']['topology']['border_spine'] = {}
+            model_data['vxlan']['topology']['border_gateway'] = {}
+            model_data['vxlan']['topology']['border_gateway_spine'] = {}
+            model_data['vxlan']['topology']['super_spine'] = {}
+            model_data['vxlan']['topology']['border_super_spine'] = {}
+            model_data['vxlan']['topology']['border_gateway_super_spine'] = {}
+            model_data['vxlan']['topology']['tor'] = {}
+            sm_switches = model_data['vxlan']['topology']['switches']
+            for switch in sm_switches:
+                # Build list of switch IP's based on role keyed by switch name
+                name = switch.get('name')
+                role = switch.get('role')
+                model_data['vxlan']['topology'][role][name] = {}
+                v4_key = 'management_ipv4_address'
+                v6_key = 'management_ipv6_address'
+                v4ip = switch.get('management').get(v4_key)
+                v6ip = switch.get('management').get(v6_key)
+                model_data['vxlan']['topology'][role][name][v4_key] = v4ip
+                model_data['vxlan']['topology'][role][name][v6_key] = v6ip
 
         self.kwargs['results']['model_extended'] = model_data
         return self.kwargs['results']
