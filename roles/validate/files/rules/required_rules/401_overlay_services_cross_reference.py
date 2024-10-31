fc128388b622bce47705c6f25f40b6f3282196e3
mwiebe <mwiebe@cisco.com>
2024-10-31 15:15:01 -0400

    Refactor and simplify code

--- roles/validate/files/rules/required_rules/401_overlay_services_cross_reference.py
+++ roles/validate/files/rules/required_rules/401_overlay_services_cross_reference.py
@@ -8,75 +8,97 @@ class Rule:
         results = []
         # Cross Reference Service Model VRFs with Service Model Networks and generate
         # an error message if a network is found that is referencing a VRF
+
+        # Check if vrfs, network and switch data is defined in the service model
+        switches = []
+        check = cls.data_model_key_check(inventory, ['vxlan', 'topology', 'switches'])
+        if 'switches' in check['keys_data']:
+            switches = inventory.get("vxlan").get("topology").get("switches")
+        if not switches:
+            # No switches defined in the service model, no reason to continue
+            return results
+
         sm_networks = []
-        if inventory.get("vxlan", None):
-            if inventory["vxlan"].get("overlay_services", None):
-                if inventory.get("vxlan").get("overlay_services").get("networks", None):
-                    sm_networks = inventory.get("vxlan").get("overlay_services").get("networks")
+        check = cls.data_model_key_check(inventory, ['vxlan', 'overlay_services', 'networks'])
+        if 'networks' in check['keys_data']:
+            sm_networks = inventory.get("vxlan").get("overlay_services").get("networks")
 
         sm_vrfs = []
-        if inventory.get("vxlan", None):
-            if inventory["vxlan"].get("overlay_services", None):
-                if inventory.get("vxlan").get("overlay_services").get("vrfs", None):
-                    sm_vrfs = inventory.get("vxlan").get("overlay_services").get("vrfs")
+        check = cls.data_model_key_check(inventory, ['vxlan', 'overlay_services', 'vrfs'])
+        if 'vrfs' in check['keys_data']:
+            sm_vrfs = inventory.get("vxlan").get("overlay_services").get("vrfs")
 
-        # Build list of VRF names from sm_vrfs
-        if sm_vrfs and sm_networks:
-            vrf_names = []
-            for vrf in sm_vrfs:
-                vrf_names.append(vrf.get("name"))
-            # Compare the two lists and generate an error message if a network is found
-            # in network_vrf_names that is not in vrf_names
-            for net in sm_networks:
-                if net.get("vrf_name") is not None:
-                    if net.get("vrf_name") not in vrf_names:
-                        # results['failed'] = True
-                        results.append(
-                            "Network ({0}) is referencing VRF ({1})".format(
-                                net.get("name"), net.get("vrf_name")
-                            )
-                        )
-                        results.append(
-                            " which is not defined in the service model.  Add the VRF to the service model or remove the network from the service model"
-                        )
-                        results.append(" and re-run the playbook")
-
-        # Cross reference VRF attach groups hostnames with inventory topology switch names
         vrf_attach_groups = []
-        if inventory.get("vxlan"):
-            if inventory.get("vxlan").get("overlay_services"):
-                if inventory.get("vxlan").get("overlay_services").get("vrf_attach_groups"):
-                    vrf_attach_groups = inventory.get("vxlan").get("overlay_services").get("vrf_attach_groups")
-                if inventory.get("vxlan").get("topology"):
-                    if inventory.get("vxlan").get("topology").get("switches"):
-                        switches = inventory.get("vxlan").get("topology").get("switches")
-        for vrf_attach_group in vrf_attach_groups:
-            for switch in vrf_attach_group.get("switches"):
-                if switch.get("hostname"):
-                    if not any(s.get("name") == switch.get("hostname") for s in switches):
-                        if not any(s.get('management').get('management_ipv4_address') == switch.get("hostname") for s in switches):
-                            if not any(s.get('management').get('management_ipv6_address') == switch.get("hostname") for s in switches):
-                                vag = vrf_attach_group.get("name")
-                                hn = switch.get("hostname")
-                                results.append("VRF attach group {0} hostname {1} does not match any switch in the topology".format(vag, hn))
+        check = cls.data_model_key_check(inventory, ['vxlan', 'overlay_services', 'vrf_attach_groups'])
+        if 'vrf_attach_groups' in check['keys_data']:
+            vrf_attach_groups = inventory.get("vxlan").get("overlay_services").get("vrf_attach_groups")
 
-        # Cross reference Network attach groups hostnames with inventory topology switch names
         network_attach_groups = []
-        if inventory.get("vxlan"):
-            if inventory.get("vxlan").get("overlay_services"):
-                if inventory.get("vxlan").get("overlay_services").get("network_attach_groups"):
-                    network_attach_groups = inventory.get("vxlan").get("overlay_services").get("network_attach_groups")
-                if inventory.get("vxlan").get("topology"):
-                    if inventory.get("vxlan").get("topology").get("switches"):
-                        switches = inventory.get("vxlan").get("topology").get("switches")
-        for network_attach_group in network_attach_groups:
-            for switch in network_attach_group.get("switches"):
+        check = cls.data_model_key_check(inventory, ['vxlan', 'overlay_services', 'network_attach_groups'])
+        if 'network_attach_groups' in check['keys_data']:
+            network_attach_groups = inventory.get("vxlan").get("overlay_services").get("network_attach_groups")
+
+        # Ensure Network is not referencing a VRF that is not defined in the service model
+        results = cls.cross_reference_vrfs_nets(sm_vrfs, sm_networks, results)
+
+        if sm_vrfs and vrf_attach_groups:
+            results = cls.cross_reference_switches(vrf_attach_groups, switches, 'vrf', results)
+        if sm_networks and network_attach_groups:
+            results = cls.cross_reference_switches(network_attach_groups, switches, 'vrf', results)
+
+        return results
+
+    @classmethod
+    def data_model_key_check(cls, tested_object, keys):
+        dm_key_dict = {'keys_found': [], 'keys_not_found': [], 'keys_data': [], 'keys_no_data': []}
+        for key in keys:
+            if tested_object and key in tested_object:
+                dm_key_dict['keys_found'].append(key)
+                tested_object = tested_object[key]
+                if tested_object:
+                    dm_key_dict['keys_data'].append(key)
+                else:
+                    dm_key_dict['keys_no_data'].append(key)
+            else:
+                dm_key_dict['keys_not_found'].append(key)
+        return dm_key_dict
+
+    @classmethod
+    def cross_reference_vrfs_nets(cls, sm_vrfs, sm_networks, results):
+        if not sm_vrfs or not sm_networks:
+            return results
+
+        vrf_names = []
+        for vrf in sm_vrfs:
+            vrf_names.append(vrf.get("name"))
+        # Compare the two lists and generate an error message if a network is found
+        # in network_vrf_names that is not in vrf_names
+        for net in sm_networks:
+            if net.get("vrf_name") is not None:
+                if net.get("vrf_name") not in vrf_names:
+                    results.append(
+                        "Network ({0}) is referencing VRF ({1})".format(
+                            net.get("name"), net.get("vrf_name")
+                        )
+                    )
+                    results.append(
+                        " which is not defined in the service model.  Add the VRF to the service model or remove the network from the service model"
+                    )
+                    results.append(" and re-run the playbook")
+
+        return results
+
+    @classmethod
+    def cross_reference_switches(cls, attach_groups, switches, target, results):
+        # target is either vrf or network
+        for attach_group in attach_groups:
+            for switch in attach_group.get("switches"):
                 if switch.get("hostname"):
                     if not any(s.get("name") == switch.get("hostname") for s in switches):
                         if not any(s.get('management').get('management_ipv4_address') == switch.get("hostname") for s in switches):
                             if not any(s.get('management').get('management_ipv6_address') == switch.get("hostname") for s in switches):
-                                nag = network_attach_group.get("name")
+                                ag = attach_group.get("name")
                                 hn = switch.get("hostname")
-                                results.append("Network attach group {0} hostname {1} does not match any switch in the topology".format(nag, hn))
+                                results.append("{0} attach group {1} hostname {2} does not match any switch in the topology".format(target, ag, hn))
 
         return results
