1803a1e2a61c1a4c008c1a74f232b53a496c67da
mikewiebe <mwiebe@cisco.com>
2024-11-07 19:40:12 +0000

    More tag refactoring

--- plugins/action/common/check_roles.py
+++ plugins/action/common/check_roles.py
@@ -38,7 +38,7 @@ class ActionModule(ActionBase):
         results['save_previous'] = False
 
         roles = self._task.args['role_list']
-        for role in ['cisco.nac_dc_vxlan.create', 'cisco.nac_dc_vxlan.remove']:
+        for role in ['cisco.nac_dc_vxlan.create', 'cisco.nac_dc_vxlan.deploy', 'cisco.nac_dc_vxlan.remove']:
             if role in roles:
                 results['save_previous'] = True
 
