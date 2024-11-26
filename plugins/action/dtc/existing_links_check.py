8e0d50a4c5b4e9db56f4a9d432988e2b1ebe6c80
Matt Thurston <mthursto@cisco.com>
2024-11-26 09:51:08 +0000

    removed trailing whitespace

--- plugins/action/dtc/existing_links_check.py
+++ plugins/action/dtc/existing_links_check.py
@@ -44,7 +44,7 @@ class ActionModule(ActionBase):
                     existing_link['sw1-info']['if-name'] == link['src_interface'] and
                     existing_link['sw2-info']['sw-sys-name'] == link['dst_device'] and
                     existing_link['sw2-info']['if-name'] == link['dst_interface'])):
-                    not_required_links.append(link)          
+                    not_required_links.append(link)
         results['not_required_links'] = not_required_links
 
         return results
