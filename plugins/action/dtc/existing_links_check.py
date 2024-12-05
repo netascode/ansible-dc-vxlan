b09c73d15c3a53de9a41f532b701fbe1f2f4ae6c
Matt Thurston <mthursto@cisco.com>
2024-12-05 16:05:02 +0000

    Update to make check work in both directions and to lower case the interface

--- plugins/action/dtc/existing_links_check.py
+++ plugins/action/dtc/existing_links_check.py
@@ -41,9 +41,13 @@ class ActionModule(ActionBase):
             for existing_link in existing_links:
                 if ('sw1-info' in existing_link and 'sw2-info' in existing_link and
                     (existing_link['sw1-info']['sw-sys-name'] == link['src_device'] and
-                     existing_link['sw1-info']['if-name'] == link['src_interface'] and
+                     existing_link['sw1-info']['if-name'].lower() == link['src_interface'] and
                      existing_link['sw2-info']['sw-sys-name'] == link['dst_device'] and
-                     existing_link['sw2-info']['if-name'] == link['dst_interface'])):
+                     existing_link['sw2-info']['if-name'].lower() == link['dst_interface']) or
+                    (existing_link['sw1-info']['sw-sys-name'] == link['dst_device'] and
+                     existing_link['sw1-info']['if-name'].lower() == link['dst_interface'] and
+                     existing_link['sw2-info']['sw-sys-name'] == link['src_device'] and
+                     existing_link['sw2-info']['if-name'].lower() == link['src_interface'])):
                     not_required_links.append(link)
         results['not_required_links'] = not_required_links
 
