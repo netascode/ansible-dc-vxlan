972e3c0bf43588d9da8c05d4d9797ccadbff26da
Matt Tarkington <mtarking@cisco.com>
2025-02-24 14:56:02 -0500

    refactor line lengths

--- plugins/action/dtc/manage_child_fabric_networks.py
+++ plugins/action/dtc/manage_child_fabric_networks.py
@@ -55,12 +55,12 @@ class ActionModule(ActionBase):
 
         for network in networks:
             network_attach_group_switches = [
-                network_attach_groups_dict['switches'] 
-                for network_attach_groups_dict in network_attach_groups_dict 
+                network_attach_groups_dict['switches']
+                for network_attach_groups_dict in network_attach_groups_dict
                 if network_attach_groups_dict['name'] == network['network_attach_group']
             ][0]
             network_attach_group_switches_mgmt_ip_addresses = [
-                network_attach_group_switch['mgmt_ip_address'] 
+                network_attach_group_switch['mgmt_ip_address']
                 for network_attach_group_switch in network_attach_group_switches
             ]
 
@@ -77,25 +77,35 @@ class ActionModule(ActionBase):
                     # Need to clean these up and make them more dynamic
                     if network.get('netflow_enable'):
                         if child_fabric_attributes['ENABLE_NETFLOW'] == 'false':
-                            display.error(f"For fabric {child_fabric}; NetFlow is not enabled in the fabric settings. To enable NetFlow in VRF, you need to first enable it in the fabric settings.")
+                            error_msg = (
+                                f"For fabric {child_fabric}; NetFlow is not enabled in the fabric settings. "
+                                "To enable NetFlow in Network, you need to first enable it in the fabric settings."
+                            )
+                            display.error(error_msg)
                             results['failed'] = True
-                            results['msg'] = f"For fabric {child_fabric}; NetFlow is not enabled in the fabric settings. To enable NetFlow in VRF, you need to first enable it in the fabric settings."
+                            results['msg'] = error_msg
                             return results
 
                     if network.get('trm_enable'):
                         if child_fabric_attributes['ENABLE_TRM'] == 'false':
-                            display.error(f"For fabric {child_fabric}; TRM is not enabled in the fabric settings. To enable TRM in VRF, you need to first enable it in the fabric settings.")
+                            error_msg = (f"For fabric {child_fabric}; TRM is not enabled in the fabric settings. "
+                                         "To enable TRM in Network, you need to first enable it in the fabric settings."
+                            )
+                            display.error(error_msg)
                             results['failed'] = True
-                            results['msg'] = f"For fabric {child_fabric}; TRM is not enabled in the fabric settings. To enable TRM in VRF, you need to first enable it in the fabric settings."
+                            results['msg'] = error_msg
                             return results
 
                     # Need to check data model and support for enabling TRMv6 in the fabric settings
-                    # if vrf.get('trm_enable'):
+                    # if network.get('trm_enable'):
                     #     if child_fabric_attributes['ENABLE_TRMv6'] == 'false':
-                    #         # Ansible Display failure
-                    #         display.error(f"For fabric {child_fabric}; TRMv6 is not enabled in the fabric settings. To enable TRMv6 in VRF, you need to first enable it in the fabric settings.")
+                    #         error_msg = (f"For fabric {child_fabric}; TRMv6 is not enabled in the fabric settings. "
+                    #                      "To enable TRMv6 in Network, you need to first enable it in the fabric settings."
+                    #         )
+                    #         display.error(error_msg)
                     #         results['failed'] = True
-                    #         results['msg'] = f"For fabric {child_fabric}; TRMv6 is not enabled in the fabric settings. To enable TRMv6 in VRF, you need to first enable it in the fabric settings."
+                    #         results['msg'] = error_msg
+                    #         return results
 
                     ndfc_net = self._execute_module(
                         module_name="cisco.dcnm.dcnm_rest",
@@ -155,15 +165,15 @@ class ActionModule(ActionBase):
                     #     "response": {
                     #         "RETURN_CODE": 200,
                     #         "METHOD": "PUT",
-                    #         "REQUEST_PATH": "https://10.15.0.110:443/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric1/vrfs/NaC-VRF01",
+                    #         "REQUEST_PATH": "https://10.15.0.110:443/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric1/vrfs/NaC-VRF01", # noqa: E501
                     #         "MESSAGE": "OK",
-                    #         "DATA": {     
+                    #         "DATA": {
                     #         }
                     #     },
                     #     "invocation": {
                     #         "module_args": {
                     #             "method": "PUT",
-                    #             "path": "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric1/vrfs/NaC-VRF01",
+                    #             "path": "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric1/vrfs/NaC-VRF01", # noqa: E501
                     #             "data": "{\n    \"displayName\": \"NaC-VRF01\",\n    \"fabric\": \"nac-fabric1\",\n    \"hierarchicalKey\": \"nac-fabric1\",\n    \"vrfExtensionTemplate\": \"Default_VRF_Extension_Universal\",\n    \"vrfId\": \"150001\",\n    \"vrfName\": \"NaC-VRF01\",\n    \"vrfTemplate\": \"Default_VRF_Universal\",\n    \"vrfTemplateConfig\": \"{\\'asn\\': \\'65001\\', \\'nveId\\': \\'1\\', \\'vrfName\\': \\'NaC-VRF01\\', \\'vrfSegmentId\\': \\'150001\\', \\'vrfVlanId\\': \\'2001\\', \\'vrfVlanName\\': \\'\\', \\'vrfDescription\\': \\'Configured by Ansible NetAsCode\\', \\'vrfIntfDescription\\': \\'Configured by Ansible NetAsCode\\', \\'mtu\\': \\'9216\\', \\'tag\\': \\'12345\\', \\'vrfRouteMap\\': \\'FABRIC-RMAP-REDIST-SUBNET\\', \\'v6VrfRouteMap\\': \\'\\', \\'maxBgpPaths\\': \\'1\\', \\'maxIbgpPaths\\': \\'2\\', \\'ipv6LinkLocalFlag\\': \\'true\\', \\'enableL3VniNoVlan\\': \\'false\\', \\'enableBgpBestPathEcmp\\': \\'false\\', \\'advertiseHostRouteFlag\\': \\'false\\', \\'advertiseDefaultRouteFlag\\': \\'true\\', \\'configureStaticDefaultRouteFlag\\': \\'true\\', \\'bgpPassword\\': \\'\\', \\'bgpPasswordKeyType\\': \\'3\\', \\'ENABLE_NETFLOW\\': \\'false\\', \\'NETFLOW_MONITOR\\': \\'\\', \\'trmEnabled\\': True, \\'loopbackNumber\\': 1002, \\'rpAddress\\': \\'100.100.100.1\\', \\'isRPAbsent\\': False, \\'isRPExternal\\': False, \\'L3VniMcastGroup\\': \\'239.1.1.0\\', \\'multicastGroup\\': \\'224.0.0.0/4\\', \\'trmV6Enabled\\': \\'false\\', \\'rpV6Address\\': \\'\\', \\'isV6RPAbsent\\': \\'false\\', \\'isV6RPExternal\\': \\'false\\', \\'ipv6MulticastGroup\\': \\'\\', \\'disableRtAuto\\': \\'false\\', \\'routeTargetImport\\': \\'\\', \\'routeTargetExport\\': \\'\\', \\'routeTargetImportEvpn\\': \\'\\', \\'routeTargetExportEvpn\\': \\'\\', \\'routeTargetImportMvpn\\': \\'\\', \\'routeTargetExportMvpn\\': \\'\\', \\'mvpnInterAs\\': \\'false\\', \\'trmBGWMSiteEnabled\\': True}\",\n    \"vrfVlanId\": \"2001\"\n}" # noqa: E501
                     #         }
                     #     },
@@ -180,12 +190,12 @@ class ActionModule(ActionBase):
                     #     "msg":{
                     #         "RETURN_CODE": 400,
                     #         "METHOD": "PUT",
-                    #         "REQUEST_PATH": "https://10.15.0.110:443/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric2/vrfs/NaC-VRF01",
+                    #         "REQUEST_PATH": "https://10.15.0.110:443/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric2/vrfs/NaC-VRF01", # noqa: E501
                     #         "MESSAGE": "Bad Request",
                     #         "DATA": {
                     #             "path": "/rest/top-down/fabrics/nac-fabric2/vrfs/NaC-VRF01",
                     #             "Error": "Bad Request Error",
-                    #             "message": "TRM is not enabled in the fabric settings. To enable TRM in VRF, you need to first enable it in the fabric settings.",
+                    #             "message": "TRM is not enabled in the fabric settings. To enable TRM in VRF, you need to first enable it in the fabric settings.", # noqa: E501
                     #             "timestamp": "2025-02-24 13:49:41.024",
                     #             "status": "400"
                     #         }
@@ -193,7 +203,7 @@ class ActionModule(ActionBase):
                     #     "invocation": {
                     #         "module_args": {
                     #             "method": "PUT",
-                    #             "path": "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric2/vrfs/NaC-VRF01",
+                    #             "path": "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric2/vrfs/NaC-VRF01", # noqa: E501
                     #             "data": "{\n    \"displayName\": \"NaC-VRF01\",\n    \"fabric\": \"nac-fabric2\",\n    \"hierarchicalKey\": \"nac-fabric2\",\n    \"vrfExtensionTemplate\": \"Default_VRF_Extension_Universal\",\n    \"vrfId\": \"150001\",\n    \"vrfName\": \"NaC-VRF01\",\n    \"vrfTemplate\": \"Default_VRF_Universal\",\n    \"vrfTemplateConfig\": \"{\\'asn\\': \\'65002\\', \\'nveId\\': \\'1\\', \\'vrfName\\': \\'NaC-VRF01\\', \\'vrfSegmentId\\': \\'150001\\', \\'vrfVlanId\\': \\'2001\\', \\'vrfVlanName\\': \\'\\', \\'vrfDescription\\': \\'Configured by Ansible NetAsCode\\', \\'vrfIntfDescription\\': \\'Configured by Ansible NetAsCode\\', \\'mtu\\': \\'9216\\', \\'tag\\': \\'12345\\', \\'vrfRouteMap\\': \\'FABRIC-RMAP-REDIST-SUBNET\\', \\'v6VrfRouteMap\\': \\'\\', \\'maxBgpPaths\\': \\'1\\', \\'maxIbgpPaths\\': \\'2\\', \\'ipv6LinkLocalFlag\\': \\'true\\', \\'enableL3VniNoVlan\\': \\'false\\', \\'enableBgpBestPathEcmp\\': \\'false\\', \\'advertiseHostRouteFlag\\': \\'false\\', \\'advertiseDefaultRouteFlag\\': \\'true\\', \\'configureStaticDefaultRouteFlag\\': \\'true\\', \\'bgpPassword\\': \\'\\', \\'bgpPasswordKeyType\\': \\'3\\', \\'ENABLE_NETFLOW\\': \\'false\\', \\'NETFLOW_MONITOR\\': \\'\\', \\'trmEnabled\\': True, \\'loopbackNumber\\': 1002, \\'rpAddress\\': \\'100.100.100.1\\', \\'isRPAbsent\\': False, \\'isRPExternal\\': False, \\'L3VniMcastGroup\\': \\'239.1.1.0\\', \\'multicastGroup\\': \\'224.0.0.0/4\\', \\'trmV6Enabled\\': \\'false\\', \\'rpV6Address\\': \\'\\', \\'isV6RPAbsent\\': \\'false\\', \\'isV6RPExternal\\': \\'false\\', \\'ipv6MulticastGroup\\': \\'\\', \\'disableRtAuto\\': \\'false\\', \\'routeTargetImport\\': \\'\\', \\'routeTargetExport\\': \\'\\', \\'routeTargetImportEvpn\\': \\'\\', \\'routeTargetExportEvpn\\': \\'\\', \\'routeTargetImportMvpn\\': \\'\\', \\'routeTargetExportMvpn\\': \\'\\', \\'mvpnInterAs\\': \\'false\\', \\'trmBGWMSiteEnabled\\': True}\",\n    \"vrfVlanId\": \"2001\"\n}" # noqa: E501
                     #         }
                     #     },
--- plugins/action/dtc/manage_child_fabric_vrfs.py
+++ plugins/action/dtc/manage_child_fabric_vrfs.py
@@ -67,25 +67,35 @@ class ActionModule(ActionBase):
                     # Need to clean these up and make them more dynamic
                     if vrf.get('netflow_enable'):
                         if child_fabric_attributes['ENABLE_NETFLOW'] == 'false':
-                            display.error(f"For fabric {child_fabric}; NetFlow is not enabled in the fabric settings. To enable NetFlow in VRF, you need to first enable it in the fabric settings.")
+                            error_msg = (
+                                f"For fabric {child_fabric}; NetFlow is not enabled in the fabric settings. "
+                                "To enable NetFlow in VRF, you need to first enable it in the fabric settings."
+                            )
+                            display.error(error_msg)
                             results['failed'] = True
-                            results['msg'] = f"For fabric {child_fabric}; NetFlow is not enabled in the fabric settings. To enable NetFlow in VRF, you need to first enable it in the fabric settings."
+                            results['msg'] = error_msg
                             return results
 
                     if vrf.get('trm_enable'):
                         if child_fabric_attributes['ENABLE_TRM'] == 'false':
-                            display.error(f"For fabric {child_fabric}; TRM is not enabled in the fabric settings. To enable TRM in VRF, you need to first enable it in the fabric settings.")
+                            error_msg = (f"For fabric {child_fabric}; TRM is not enabled in the fabric settings. "
+                                         "To enable TRM in VRF, you need to first enable it in the fabric settings."
+                            )
+                            display.error(error_msg)
                             results['failed'] = True
-                            results['msg'] = f"For fabric {child_fabric}; TRM is not enabled in the fabric settings. To enable TRM in VRF, you need to first enable it in the fabric settings."
+                            results['msg'] = error_msg
                             return results
 
                     # Need to check data model and support for enabling TRMv6 in the fabric settings
                     # if vrf.get('trm_enable'):
                     #     if child_fabric_attributes['ENABLE_TRMv6'] == 'false':
-                    #         # Ansible Display failure
-                    #         display.error(f"For fabric {child_fabric}; TRMv6 is not enabled in the fabric settings. To enable TRMv6 in VRF, you need to first enable it in the fabric settings.")
+                    #         error_msg = (f"For fabric {child_fabric}; TRMv6 is not enabled in the fabric settings. "
+                    #                      "To enable TRMv6 in VRF, you need to first enable it in the fabric settings."
+                    #         )
+                    #         display.error(error_msg)
                     #         results['failed'] = True
-                    #         results['msg'] = f"For fabric {child_fabric}; TRMv6 is not enabled in the fabric settings. To enable TRMv6 in VRF, you need to first enable it in the fabric settings."
+                    #         results['msg'] = error_msg
+                    #         return results
 
                     ndfc_vrf = self._execute_module(
                         module_name="cisco.dcnm.dcnm_rest",
@@ -141,7 +151,7 @@ class ActionModule(ActionBase):
                     #     "response": {
                     #         "RETURN_CODE": 200,
                     #         "METHOD": "PUT",
-                    #         "REQUEST_PATH": "https://10.15.0.110:443/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric1/vrfs/NaC-VRF01",
+                    #         "REQUEST_PATH": "https://10.15.0.110:443/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric1/vrfs/NaC-VRF01", # noqa: E501
                     #         "MESSAGE": "OK",
                     #         "DATA": {
                     #         }
@@ -149,7 +159,7 @@ class ActionModule(ActionBase):
                     #     "invocation": {
                     #         "module_args": {
                     #             "method": "PUT",
-                    #             "path": "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric1/vrfs/NaC-VRF01",
+                    #             "path": "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric1/vrfs/NaC-VRF01", # noqa: E501
                     #             "data": "{\n    \"displayName\": \"NaC-VRF01\",\n    \"fabric\": \"nac-fabric1\",\n    \"hierarchicalKey\": \"nac-fabric1\",\n    \"vrfExtensionTemplate\": \"Default_VRF_Extension_Universal\",\n    \"vrfId\": \"150001\",\n    \"vrfName\": \"NaC-VRF01\",\n    \"vrfTemplate\": \"Default_VRF_Universal\",\n    \"vrfTemplateConfig\": \"{\\'asn\\': \\'65001\\', \\'nveId\\': \\'1\\', \\'vrfName\\': \\'NaC-VRF01\\', \\'vrfSegmentId\\': \\'150001\\', \\'vrfVlanId\\': \\'2001\\', \\'vrfVlanName\\': \\'\\', \\'vrfDescription\\': \\'Configured by Ansible NetAsCode\\', \\'vrfIntfDescription\\': \\'Configured by Ansible NetAsCode\\', \\'mtu\\': \\'9216\\', \\'tag\\': \\'12345\\', \\'vrfRouteMap\\': \\'FABRIC-RMAP-REDIST-SUBNET\\', \\'v6VrfRouteMap\\': \\'\\', \\'maxBgpPaths\\': \\'1\\', \\'maxIbgpPaths\\': \\'2\\', \\'ipv6LinkLocalFlag\\': \\'true\\', \\'enableL3VniNoVlan\\': \\'false\\', \\'enableBgpBestPathEcmp\\': \\'false\\', \\'advertiseHostRouteFlag\\': \\'false\\', \\'advertiseDefaultRouteFlag\\': \\'true\\', \\'configureStaticDefaultRouteFlag\\': \\'true\\', \\'bgpPassword\\': \\'\\', \\'bgpPasswordKeyType\\': \\'3\\', \\'ENABLE_NETFLOW\\': \\'false\\', \\'NETFLOW_MONITOR\\': \\'\\', \\'trmEnabled\\': True, \\'loopbackNumber\\': 1002, \\'rpAddress\\': \\'100.100.100.1\\', \\'isRPAbsent\\': False, \\'isRPExternal\\': False, \\'L3VniMcastGroup\\': \\'239.1.1.0\\', \\'multicastGroup\\': \\'224.0.0.0/4\\', \\'trmV6Enabled\\': \\'false\\', \\'rpV6Address\\': \\'\\', \\'isV6RPAbsent\\': \\'false\\', \\'isV6RPExternal\\': \\'false\\', \\'ipv6MulticastGroup\\': \\'\\', \\'disableRtAuto\\': \\'false\\', \\'routeTargetImport\\': \\'\\', \\'routeTargetExport\\': \\'\\', \\'routeTargetImportEvpn\\': \\'\\', \\'routeTargetExportEvpn\\': \\'\\', \\'routeTargetImportMvpn\\': \\'\\', \\'routeTargetExportMvpn\\': \\'\\', \\'mvpnInterAs\\': \\'false\\', \\'trmBGWMSiteEnabled\\': True}\",\n    \"vrfVlanId\": \"2001\"\n}" # noqa: E501
                     #         }
                     #     },
@@ -166,12 +176,12 @@ class ActionModule(ActionBase):
                     #     "msg":{
                     #         "RETURN_CODE": 400,
                     #         "METHOD": "PUT",
-                    #         "REQUEST_PATH": "https://10.15.0.110:443/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric2/vrfs/NaC-VRF01",
+                    #         "REQUEST_PATH": "https://10.15.0.110:443/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric2/vrfs/NaC-VRF01", # noqa: E501
                     #         "MESSAGE": "Bad Request",
                     #         "DATA": {
                     #             "path": "/rest/top-down/fabrics/nac-fabric2/vrfs/NaC-VRF01",
                     #             "Error": "Bad Request Error",
-                    #             "message": "TRM is not enabled in the fabric settings. To enable TRM in VRF, you need to first enable it in the fabric settings.",
+                    #             "message": "TRM is not enabled in the fabric settings. To enable TRM in VRF, you need to first enable it in the fabric settings.", # noqa: E501
                     #             "timestamp": "2025-02-24 13:49:41.024",
                     #             "status": "400"
                     #         }
@@ -179,7 +189,7 @@ class ActionModule(ActionBase):
                     #     "invocation": {
                     #         "module_args": {
                     #             "method": "PUT",
-                    #             "path": "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric2/vrfs/NaC-VRF01",
+                    #             "path": "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/nac-fabric2/vrfs/NaC-VRF01", # noqa: E501
                     #             "data": "{\n    \"displayName\": \"NaC-VRF01\",\n    \"fabric\": \"nac-fabric2\",\n    \"hierarchicalKey\": \"nac-fabric2\",\n    \"vrfExtensionTemplate\": \"Default_VRF_Extension_Universal\",\n    \"vrfId\": \"150001\",\n    \"vrfName\": \"NaC-VRF01\",\n    \"vrfTemplate\": \"Default_VRF_Universal\",\n    \"vrfTemplateConfig\": \"{\\'asn\\': \\'65002\\', \\'nveId\\': \\'1\\', \\'vrfName\\': \\'NaC-VRF01\\', \\'vrfSegmentId\\': \\'150001\\', \\'vrfVlanId\\': \\'2001\\', \\'vrfVlanName\\': \\'\\', \\'vrfDescription\\': \\'Configured by Ansible NetAsCode\\', \\'vrfIntfDescription\\': \\'Configured by Ansible NetAsCode\\', \\'mtu\\': \\'9216\\', \\'tag\\': \\'12345\\', \\'vrfRouteMap\\': \\'FABRIC-RMAP-REDIST-SUBNET\\', \\'v6VrfRouteMap\\': \\'\\', \\'maxBgpPaths\\': \\'1\\', \\'maxIbgpPaths\\': \\'2\\', \\'ipv6LinkLocalFlag\\': \\'true\\', \\'enableL3VniNoVlan\\': \\'false\\', \\'enableBgpBestPathEcmp\\': \\'false\\', \\'advertiseHostRouteFlag\\': \\'false\\', \\'advertiseDefaultRouteFlag\\': \\'true\\', \\'configureStaticDefaultRouteFlag\\': \\'true\\', \\'bgpPassword\\': \\'\\', \\'bgpPasswordKeyType\\': \\'3\\', \\'ENABLE_NETFLOW\\': \\'false\\', \\'NETFLOW_MONITOR\\': \\'\\', \\'trmEnabled\\': True, \\'loopbackNumber\\': 1002, \\'rpAddress\\': \\'100.100.100.1\\', \\'isRPAbsent\\': False, \\'isRPExternal\\': False, \\'L3VniMcastGroup\\': \\'239.1.1.0\\', \\'multicastGroup\\': \\'224.0.0.0/4\\', \\'trmV6Enabled\\': \\'false\\', \\'rpV6Address\\': \\'\\', \\'isV6RPAbsent\\': \\'false\\', \\'isV6RPExternal\\': \\'false\\', \\'ipv6MulticastGroup\\': \\'\\', \\'disableRtAuto\\': \\'false\\', \\'routeTargetImport\\': \\'\\', \\'routeTargetExport\\': \\'\\', \\'routeTargetImportEvpn\\': \\'\\', \\'routeTargetExportEvpn\\': \\'\\', \\'routeTargetImportMvpn\\': \\'\\', \\'routeTargetExportMvpn\\': \\'\\', \\'mvpnInterAs\\': \\'false\\', \\'trmBGWMSiteEnabled\\': True}\",\n    \"vrfVlanId\": \"2001\"\n}" # noqa: E501
                     #         }
                     #     },
