05e9ffdd2de968845816e86530aa956b0bca1124
Matt Thurston <mthursto@cisco.com>
2025-05-02 11:12:44 +0100

    Update links_filter_and_remove.py

--- plugins/action/dtc/links_filter_and_remove.py
+++ plugins/action/dtc/links_filter_and_remove.py
@@ -52,13 +52,13 @@ class ActionModule(ActionBase):
                     if ('sw1-info' in existing_link and 'sw2-info' in existing_link and
                         'sw-sys-name' in existing_link['sw1-info'] and 'sw-sys-name' in existing_link['sw2-info'] and
                         (existing_link['sw1-info']['sw-sys-name'].lower() == link['src_device'].lower() and
-                        existing_link['sw1-info']['if-name'].lower() == link['src_interface'].lower() and
-                        existing_link['sw2-info']['sw-sys-name'].lower() == link['dst_device'].lower() and
-                        existing_link['sw2-info']['if-name'].lower() == link['dst_interface'].lower()) or
+                         existing_link['sw1-info']['if-name'].lower() == link['src_interface'].lower() and
+                         existing_link['sw2-info']['sw-sys-name'].lower() == link['dst_device'].lower() and
+                         existing_link['sw2-info']['if-name'].lower() == link['dst_interface'].lower()) or
                         (existing_link['sw1-info']['sw-sys-name'].lower() == link['dst_device'].lower() and
-                        existing_link['sw1-info']['if-name'].lower() == link['dst_interface'].lower() and
-                        existing_link['sw2-info']['sw-sys-name'].lower() == link['src_device'].lower() and
-                        existing_link['sw2-info']['if-name'].lower() == link['src_interface'].lower())):
+                         existing_link['sw1-info']['if-name'].lower() == link['dst_interface'].lower() and
+                         existing_link['sw2-info']['sw-sys-name'].lower() == link['src_device'].lower() and
+                         existing_link['sw2-info']['if-name'].lower() == link['src_interface'].lower())):
                         required_links.append(existing_link)
         for link in filtered_existing_links:
             link_found = False
