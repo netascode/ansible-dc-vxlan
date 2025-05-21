6c1536523422af008d7a5ed50e589ebbc62eb537
Mike Wiebe <mwiebe@cisco.com>
2025-05-21 10:03:15 -0400

    Fix missing preprovision validate role key error (#394)
    * Fix missing preprovision key
    
    * Fix if condition

--- roles/validate/files/rules/vxlan/307_topology_switch_preprovision.py
+++ roles/validate/files/rules/vxlan/307_topology_switch_preprovision.py
@@ -7,14 +7,32 @@ class Rule:
     def match(cls, inventory):
         results = []
         switches = []
-        if inventory.get("vxlan", None):
-            if inventory["vxlan"].get("topology", None):
-                if inventory.get("vxlan").get("topology").get("switches", None):
-                    switches = inventory.get("vxlan").get("topology").get("switches")
+
+        dm_check = cls.data_model_key_check(inventory, ['vxlan', 'topology', 'switches'])
+        if 'switches' in dm_check['keys_data']:
+            switches = inventory['vxlan']['topology']['switches']
+
         for switch in switches:
-            if switch.get("poap").get("preprovision", None):
-                if not switch.get("management").get("subnet_mask_ipv4"):
+            dm_check = cls.data_model_key_check(switch, ['poap', 'preprovision'])
+            if 'preprovision' in dm_check['keys_data']:
+                dm_check = cls.data_model_key_check(switch, ['management', 'subnet_mask_ipv4'])
+                if 'subnet_mask_ipv4' in dm_check['keys_not_found'] or 'subnet_mask_ipv4' in dm_check['keys_no_data']:
                     results.append(
                         f"vxlan.topology.switches.{switch['name']}.subnet_mask_ipv4 must be defined when preprovision is used."
                     )
         return results
+
+    @classmethod
+    def data_model_key_check(cls, tested_object, keys):
+        dm_key_dict = {'keys_found': [], 'keys_not_found': [], 'keys_data': [], 'keys_no_data': []}
+        for key in keys:
+            if tested_object and key in tested_object:
+                dm_key_dict['keys_found'].append(key)
+                tested_object = tested_object[key]
+                if tested_object:
+                    dm_key_dict['keys_data'].append(key)
+                else:
+                    dm_key_dict['keys_no_data'].append(key)
+            else:
+                dm_key_dict['keys_not_found'].append(key)
+        return dm_key_dict
