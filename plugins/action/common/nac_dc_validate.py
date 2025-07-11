d7574a7dd07db6d084d096a333eb79d95b36f3cb
Matt Tarkington <mtarking@cisco.com>
2025-07-11 16:10:42 -0400

    adjustments from testing with module updates

--- plugins/action/common/nac_dc_validate.py
+++ plugins/action/common/nac_dc_validate.py
@@ -91,7 +91,7 @@ class ActionModule(ActionBase):
             check = data_model_key_check(results['data'], parent_keys)
             if 'fabric' in check['keys_found'] and 'fabric' in check['keys_data']:
                 if 'type' in results['data']['vxlan']['fabric']:
-                    if results['data']['vxlan']['fabric']['type'] in ('VXLAN_EVPN'):
+                    if results['data']['vxlan']['fabric']['type'] in ('VXLAN_EVPN', 'eBGP_VXLAN'):
                         rules_list.append(f'{rules}vxlan/')
                     elif results['data']['vxlan']['fabric']['type'] in ('MSD', 'MCF'):
                         rules_list.append(f'{rules}multisite/')
@@ -117,7 +117,7 @@ class ActionModule(ActionBase):
                         )
                         display.deprecated(msg=deprecated_msg, version='1.0.0', collection_name='cisco.nac_dc_vxlan')
 
-                        if results['data']['vxlan']['global']['fabric_type'] in ('VXLAN_EVPN'):
+                        if results['data']['vxlan']['global']['fabric_type'] in ('VXLAN_EVPN', 'eBGP_VXLAN'):
                             rules_list.append(f'{rules}vxlan/')
                         elif results['data']['vxlan']['global']['fabric_type'] in ('MSD', 'MCF'):
                             rules_list.append(f'{rules}multisite/')
