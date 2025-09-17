6483e1824a69986d407557903199288ae28fbae8
Matt Tarkington <mtarking@cisco.com>
2025-09-17 16:51:17 -0400

    Update 200_global_bgp_asn.py

--- roles/validate/files/rules/common/200_global_bgp_asn.py
+++ roles/validate/files/rules/common/200_global_bgp_asn.py
@@ -20,10 +20,15 @@ class Rule:
         # {'keys_found': ['vxlan', 'global', 'bgp_asn'], 'keys_not_found': ['ibgp'], 'keys_data': ['vxlan', 'global'], 'keys_no_data': ['bgp_asn']}
         # {'keys_found': ['vxlan', 'global', 'ibgp', 'bgp_asn'], 'keys_not_found': [], 'keys_data': ['vxlan', 'global', 'ibgp'], 'keys_no_data': ['bgp_asn']}
         check = cls.data_model_key_check(data, ['vxlan', 'global', fabric_type, 'bgp_asn'])
-        if 'bgp_asn' not in check['keys_found']:
-            results.append(f"vxlan.global.{fabric_type}.bgp_asn must be defined in the data model.")
-        elif 'bgp_asn' in check['keys_found'] and 'bgp_asn' in check['keys_no_data']:
+        if 'bgp_asn' in check['keys_not_found']:
+            check = cls.data_model_key_check(data, ['vxlan', 'global', 'bgp_asn'])
+            if 'bgp_asn' in check['keys_not_found']:
+                results.append(f"vxlan.global.{fabric_type}.bgp_asn must be defined in the data model.")
+                return results
+
+        if 'bgp_asn' in check['keys_found'] and 'bgp_asn' in check['keys_no_data']:
             results.append(f"vxlan.global.{fabric_type}.bgp_asn must have a value defined in the data model.")
+            return results
 
         return results
 
