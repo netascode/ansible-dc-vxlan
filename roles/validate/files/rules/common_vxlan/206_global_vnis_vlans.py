ac28b787903bae7da52e1417da3e5661060df360
Matt Tarkington <mtarking@cisco.com>
2025-09-18 22:16:55 -0400

    update vni and vlan range rule

--- roles/validate/files/rules/common_vxlan/206_global_vnis_vlans.py
+++ roles/validate/files/rules/common_vxlan/206_global_vnis_vlans.py
@@ -18,8 +18,11 @@ class Rule:
         for key in ['layer2_vni_range', 'layer3_vni_range', 'layer2_vlan_range', 'layer3_vlan_range']:
             start_keys = ['vxlan', 'global', fabric_type, key, 'from']
             end_keys = ['vxlan', 'global', fabric_type, key, 'to']
-            check = cls.data_model_key_check(data_model, ['vxlan', 'global', fabric_type, key])
-            if key in check['keys_not_found']:
+            fabric_global_key = ['vxlan', 'global', fabric_type]
+            check = cls.data_model_key_check(data_model, fabric_global_key)
+            # import epdb; epdb.set_trace()
+            tmp = cls.safeget(data_model, fabric_global_key + [key])
+            if fabric_type in check['keys_not_found'] or cls.safeget(data_model, fabric_global_key + [key]) is None:
                 if fabric_type in ['ibgp']:
                     check = cls.data_model_key_check(data_model, ['vxlan', 'global', key])
                     if key in check['keys_found']:
@@ -35,8 +38,6 @@ class Rule:
 
     @classmethod
     def check_ranges(cls, start_keys, end_keys, check_key, range_type, data):
-        # start_keys = ['vxlan', 'global', check_key, 'from']
-        # end_keys = ['vxlan', 'global', check_key, 'to']
         check_start = cls.data_model_key_check(data, start_keys)
         check_end = cls.data_model_key_check(data, end_keys)
 
