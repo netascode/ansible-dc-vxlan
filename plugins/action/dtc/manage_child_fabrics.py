a464d044caad9ef2a4f140c5ce73282799f83c01
Matt Tarkington <mtarking@cisco.com>
2025-11-17 14:29:21 -0500

    refactor msite plugins to handle both msd and mcfg

--- plugins/action/dtc/manage_child_fabrics.py
+++ plugins/action/dtc/manage_child_fabrics.py
@@ -31,123 +31,119 @@ display = Display()
 
 
 class ActionModule(ActionBase):
+    """
+    Action plugin to manage Multisite child fabrics
+    in Nexus Dashboard (ND) for MSD and MCFG parent fabrics.
+    """
+    def __init__(self, *args, **kwargs):
+        super(ActionModule, self).__init__(*args, **kwargs)
+        self.tmp = None
+        self.task_vars = None
+        self.parent_fabric = None
+        self.parent_fabric_type = None
+        self.multisite_operations_map = {
+            'MSD': {
+                'add': {
+                    'method': 'POST',
+                    'path': '/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/msdAdd'
+                },
+                'remove': {
+                    'method': 'POST',
+                    'path': '/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/msdExit'
+                },
+                'data_template': '{{"destFabric":"{parent_fabric}","sourceFabric":"{child_fabric}"}}'
+            },
+            'MCFG': {
+                'add': {
+                    'method': 'PUT',
+                    'path': '/onemanage/appcenter/cisco/ndfc/api/v1/onemanage/fabrics/{parent_fabric}/members',
+                },
+                'remove': {
+                    'method': 'PUT',
+                    'path': '/onemanage/appcenter/cisco/ndfc/api/v1/onemanage/fabrics/{parent_fabric}/members',
+                },
+                'data_template': '{{"clusterName":"{cluster}","fabricName":"{child_fabric}","operation":"{operation}"}}'
+            }
+        }
+
+    def _render_data_template(self, template, child_fabric, operation):
+        """
+        Render the data template with the appropriate values.
+        """
+        if self.parent_fabric_type == 'MSD':
+            return template.format(
+                parent_fabric=self.parent_fabric,
+                child_fabric=child_fabric['name']
+            )
+        elif self.parent_fabric_type == 'MCFG':
+            return template.format(
+                cluster=child_fabric['cluster'],
+                child_fabric=child_fabric['name'],
+                operation=operation
+            )
+
+    def _manage_child_fabric_membership(self, method, path, data):
+        """
+        Manage child fabrics associated with the parent fabric
+        in Nexus Dashboard for MSD and MCFG parent fabrics.
+        """
+        resposne = self._execute_module(
+                        module_name="cisco.dcnm.dcnm_rest",
+                        module_args={
+                            "method": method,
+                            "path": path,
+                            "json_data": data
+                        },
+                        task_vars=self.task_vars,
+                        tmp=self.tmp
+                    )
+
+        return resposne
 
     def run(self, tmp=None, task_vars=None):
         results = super(ActionModule, self).run(tmp, task_vars)
         results['changed'] = False
         results['failed'] = False
-        results['child_fabrics_moved'] = False
 
-        parent_fabric = self._task.args['parent_fabric']
+        self.task_vars = task_vars
+        self.tmp = tmp
+
+        self.parent_fabric = self._task.args['parent_fabric']
+        self.parent_fabric_type = self._task.args["parent_fabric_type"]
+
         child_fabrics = self._task.args['child_fabrics']
+
         state = self._task.args['state']
 
+        if not self.multisite_operations_map.keys().__contains__(self.parent_fabric_type):
+            results['failed'] = True
+            results['msg'] = f"Unsupported parent fabric type {self.parent_fabric_type} for fabric {self.parent_fabric}. Supported types are MSD and MCFG."
+            return results
+
         if state == 'present':
-            for fabric in child_fabrics:
-                json_data = '{"destFabric":"%s","sourceFabric":"%s"}' % (parent_fabric, fabric)
-                add_fabric_result = self._execute_module(
-                    module_name="cisco.dcnm.dcnm_rest",
-                    module_args={
-                        "method": "POST",
-                        "path": "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/msdAdd",
-                        "json_data": json_data
-                    },
-                    task_vars=task_vars,
-                    tmp=tmp
-                )
-
-                if add_fabric_result.get('failed'):
-                    results['failed'] = True
-                    results['msg'] = f"{add_fabric_result['msg']['MESSAGE']}: {add_fabric_result['msg']['DATA']}"
-                    break
-
-                # If a child fabric is successfully added under an MSD fabric set a flag
-                # indicating this so that it can be used later to prevent managing VRFs
-                # and Networks.  If we dont prevent this then the VRFs and Networks could
-                # be removed as part of moving the child fabric.
-                #
-                # TBD: This flag is not actually being used currently.  Discuss with team.
-                results['child_fabrics_moved'] = True
-
-                results['changed'] = True
+            operation = 'add'
+            method = self.multisite_operations_map[self.parent_fabric_type][operation]['method']
+            path = self.multisite_operations_map[self.parent_fabric_type][operation]['path'].format(parent_fabric=self.parent_fabric)
 
         if state == 'absent':
-            for fabric in child_fabrics:
-                json_data = '{"destFabric":"%s","sourceFabric":"%s"}' % (parent_fabric, fabric)
-                remove_fabric_result = self._execute_module(
-                    module_name="cisco.dcnm.dcnm_rest",
-                    module_args={
-                        "method": "POST",
-                        "path": "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/msdExit",
-                        "json_data": json_data
-                    },
-                    task_vars=task_vars,
-                    tmp=tmp
-                )
+            operation = 'remove'
+            method = self.multisite_operations_map[self.parent_fabric_type][operation]['method']
+            path = self.multisite_operations_map[self.parent_fabric_type][operation]['path'].format(parent_fabric=self.parent_fabric)
 
-                if remove_fabric_result.get('failed'):
-                    results['failed'] = True
-                    results['msg'] = f"{remove_fabric_result['msg']['MESSAGE']}: {remove_fabric_result['msg']['DATA']}"
-                    break
+        for fabric in child_fabrics:
+            data =  self._render_data_template(
+                template=self.multisite_operations_map[self.parent_fabric_type]['data_template'],
+                child_fabric=fabric,
+                operation=operation
+            )
 
-                results['changed'] = True
+            manage_fabric_result = self._manage_child_fabric_membership(method, path, data)
+
+            if manage_fabric_result.get('failed'):
+                results['failed'] = True
+                results['msg'] = f"{manage_fabric_result['msg']['MESSAGE']}: {manage_fabric_result['msg']['DATA']}"
+                break
+
+            results['changed'] = True
 
         return results
-
-
-#   cisco.dcnm.dcnm_rest:
-#     method: POST
-#     path: /appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/msdAdd
-#     json_data: '{"destFabric":"nac-msd","sourceFabric":"nac-ndfc1"}'
-
-# https://rtp-ndfc1.cisco.com/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/msd/fabric-associations/
-# GET
-# Response:
-# "response": {
-#     "DATA": [
-#         {
-#             "fabricId": 2,
-#             "fabricName": "nac-fabric1",
-#             "fabricParent": "None",
-#             "fabricState": "standalone",
-#             "fabricTechnology": "VXLANFabric",
-#             "fabricType": "Switch_Fabric"
-#         },
-#         {
-#             "fabricId": 3,
-#             "fabricName": "nac-fabric2",
-#             "fabricParent": "nac-msd1",
-#             "fabricState": "member",
-#             "fabricTechnology": "VXLANFabric",
-#             "fabricType": "Switch_Fabric"
-#         },
-#         {
-#             "fabricId": 4,
-#             "fabricName": "nac-isn1",
-#             "fabricParent": "None",
-#             "fabricState": "standalone",
-#             "fabricTechnology": "External",
-#             "fabricType": "External"
-#         },
-#         {
-#             "fabricId": 5,
-#             "fabricName": "nac-msd1",
-#             "fabricParent": "None",
-#             "fabricState": "msd",
-#             "fabricTechnology": "VXLANFabric",
-#             "fabricType": "MSD"
-#         }
-#     ],
-#     "MESSAGE": "OK",
-#     "METHOD": "GET",
-#     "REQUEST_PATH": "https://10.15.0.110:443/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/msd/fabric-associations",
-#     "RETURN_CODE": 200
-#     }
-
-# https://rtp-ndfc1.cisco.com/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/msdAdd
-# POST
-# {"destFabric":"nac-msd","sourceFabric":"nac-ndfc1"}
-
-# https://rtp-ndfc1.cisco.com/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/msdExit
-# POST
-# {"destFabric":"nac-msd","sourceFabric":"nac-ndfc1"}
