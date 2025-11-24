61a0fcdb0e41f6a4cb59cc80421706c67749ced5
Peter Lewis <pelewis2@cisco.com>
2025-11-24 10:15:12 +0000

    update links compare for src/dst fix

--- plugins/action/dtc/existing_links_check.py
+++ plugins/action/dtc/existing_links_check.py
@@ -56,6 +56,8 @@ class ActionModule(ActionBase):
                             existing_link['sw1-info']['sw-sys-name'] = switch['management']['management_ipv4_address']
                         if existing_link['sw2-info']['sw-sys-name'].lower() == switch['name'].lower():
                             existing_link['sw2-info']['sw-sys-name'] = switch['management']['management_ipv4_address']
+
+                    # Check if the existing link matches the fabric link in either direction
                     if ((existing_link['sw1-info']['sw-sys-name'].lower() == link['src_device'].lower() and
                          existing_link['sw1-info']['if-name'].lower() == link['src_interface'].lower() and
                          existing_link['sw2-info']['sw-sys-name'].lower() == link['dst_device'].lower() and
@@ -64,11 +66,44 @@ class ActionModule(ActionBase):
                          existing_link['sw1-info']['if-name'].lower() == link['dst_interface'].lower() and
                          existing_link['sw2-info']['sw-sys-name'].lower() == link['src_device'].lower() and
                          existing_link['sw2-info']['if-name'].lower() == link['src_interface'].lower())):
+
+                        # If the link is in reverse order, swap the src and dst to match
+                        # swap also in profile peer1 and peer2
+                        # Don't swap IP addresses, bc they are assigned based on existing link
+                        # and should not change
+                        if (
+                            existing_link['sw1-info']['sw-sys-name'].lower() == link['dst_device'].lower()
+                            and existing_link['sw1-info']['if-name'].lower() == link['dst_interface'].lower()
+                            and existing_link['sw2-info']['sw-sys-name'].lower() == link['src_device'].lower()
+                            and existing_link['sw2-info']['if-name'].lower() == link['src_interface'].lower()
+                        ):
+                            link['src_device'], link['dst_device'] = link['dst_device'], link['src_device']
+                            link['src_interface'], link['dst_interface'] = link['dst_interface'], link['src_interface']
+                            # Safely swap descriptions without creating keys when absent
+                            p1_desc = link['profile'].pop('peer1_description', None)
+                            p2_desc = link['profile'].pop('peer2_description', None)
+                            if p2_desc is not None:
+                                link['profile']['peer1_description'] = p2_desc
+                            if p1_desc is not None:
+                                link['profile']['peer2_description'] = p1_desc
+                            # Safely swap freeform without creating keys when absent
+                            p1_free = link['profile'].pop('peer1_freeform', None)
+                            p2_free = link['profile'].pop('peer2_freeform', None)
+                            if p2_free is not None:
+                                link['profile']['peer1_freeform'] = p2_free
+                            if p1_free is not None:
+                                link['profile']['peer2_freeform'] = p1_free
+
                         if 'templateName' not in existing_link:
                             not_required_links.append(link)
                         elif existing_link['templateName'] == 'int_pre_provision_intra_fabric_link':
                             required_links.append(link)
                         elif existing_link['templateName'] == 'int_intra_fabric_num_link':
+                            # Populate additional fields from existing link
+                            # into the required link. IPs are assigned by ND and not managed in NaC Fabric link
+                            # at this time.
+                            # When template is defined as int_pre_provision_intra_fabric_link, template is converted
+                            # by ND to int_intra_fabric_num_link, when fabric-link is P2P and IPs are assigned.
                             link['template'] = 'int_intra_fabric_num_link'
                             link['profile']['peer1_ipv4_addr'] = existing_link['nvPairs']['PEER1_IP']
                             link['profile']['peer2_ipv4_addr'] = existing_link['nvPairs']['PEER2_IP']
@@ -86,3 +121,4 @@ class ActionModule(ActionBase):
 
         results['required_links'] = required_links
         return results
+    
