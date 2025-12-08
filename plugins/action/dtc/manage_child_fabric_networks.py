1cab041550c54ae9c60351a7b2b06cba1e6684c2
Matt Tarkington <mtarking@cisco.com>
2025-12-08 14:09:02 -0500

    clean up files

--- plugins/action/dtc/manage_child_fabric_networks.py
+++ plugins/action/dtc/manage_child_fabric_networks.py
@@ -61,7 +61,6 @@ class ActionModule(ActionBase):
 
         nd_version = self._task.args["nd_version"]
         msite_data = self._task.args["msite_data"]
-        fabric_type = self._task.args["fabric_type"]
 
         # Extract major, minor, patch and patch letter from nd_version
         # that is set in nac_dc_vxlan.dtc.connectivity_check role
@@ -89,7 +88,6 @@ class ActionModule(ActionBase):
                 child_fabric_type = child_fabrics[child_fabric]['type']
                 if child_fabric_type in ['Switch_Fabric']:
                     child_fabric_attributes = child_fabrics[child_fabric]['attributes']
-                    child_fabric_cluster = child_fabrics[child_fabric].get('cluster')
 
                     network_child_fabric = []
                     if network_child_fabrics:
@@ -141,17 +139,12 @@ class ActionModule(ActionBase):
                     #         results['failed'] = True
                     #         results['msg'] = error_msg
                     #         return results
-                    if fabric_type == 'MCFG':
-                        get_path = (f"/onepath/{child_fabric_cluster}/appcenter/cisco/ndfc/api/v1/lan-fabric/"
-                                    f"rest/top-down/fabrics/{child_fabric}/networks/{network['name']}")
-                    else:
-                        get_path = f"/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/{child_fabric}/networks/{network['name']}"
 
                     ndfc_net = self._execute_module(
                         module_name="cisco.dcnm.dcnm_rest",
                         module_args={
                             "method": "GET",
-                            "path": get_path,
+                            "path": f"/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/{child_fabric}/networks/{network['name']}",
                         },
                         task_vars=task_vars,
                         tmp=tmp
@@ -214,17 +207,11 @@ class ActionModule(ActionBase):
                         rendered_content = templar.template(template_content)
                         rendered_to_nice_json = templar.environment.filters['to_nice_json'](rendered_content)
 
-                        if fabric_type == 'MCFG':
-                            put_path = (f"/onepath/{child_fabric_cluster}/appcenter/cisco/ndfc/api/v1/lan-fabric/"
-                                        f"rest/top-down/fabrics/{child_fabric}/networks/{network['name']}")
-                        else:
-                            put_path = f"/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/{child_fabric}/networks/{network['name']}"
-
                         ndfc_net_update = self._execute_module(
                             module_name="cisco.dcnm.dcnm_rest",
                             module_args={
                                 "method": "PUT",
-                                "path": put_path,
+                                "path": f"/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/{child_fabric}/networks/{network['name']}",
                                 "data": rendered_to_nice_json
                             },
                             task_vars=task_vars,
@@ -254,6 +241,7 @@ class ActionModule(ActionBase):
                         if ndfc_net_update.get('response'):
                             if ndfc_net_update['response']['RETURN_CODE'] == 200:
                                 results['changed'] = True
+
                         # Failed response:
                         # {
                         #     "failed": true,
