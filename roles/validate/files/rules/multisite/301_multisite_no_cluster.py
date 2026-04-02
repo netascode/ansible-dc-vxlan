d9f0a193a850f5ed61c65721afe26e3b5b756835
Miguel Corona <miguel_corona@hotmail.com.com>
2026-04-02 01:24:29 -0600

    Add rule to ensure that 'cluster' is not used under msite child_fabrics

---
+++ roles/validate/files/rules/multisite/301_multisite_no_cluster.py
@@ -0,0 +1,48 @@
+class Rule:
+    id = "301"
+    description = "Verify multisite fabric does not reference clusters"
+    severity = "LOW"
+
+    msg = "{0} multisite child fabric should not reference clusters"
+
+    @classmethod
+    def match(cls, data_model):
+        results = []
+
+        multisite_child_fabric_keys = ['vxlan', 'multisite', 'child_fabrics']
+        check = cls.data_model_key_check(data_model, multisite_child_fabric_keys)
+        if 'child_fabrics' in check['keys_found'] and 'child_fabrics' in check['keys_data']:
+            dm_child_fabrics = cls.safeget(data_model, multisite_child_fabric_keys)
+            for child_fabric in dm_child_fabrics:
+                if child_fabric.get('cluster', False):
+                    results.append(cls.msg.format(child_fabric['name']))
+
+        return results
+
+    @classmethod
+    def data_model_key_check(cls, tested_object, keys):
+        dm_key_dict = {'keys_found': [], 'keys_not_found': [], 'keys_data': [], 'keys_no_data': []}
+        for key in keys:
+            if tested_object and key in tested_object:
+                dm_key_dict['keys_found'].append(key)
+                tested_object = tested_object[key]
+                if tested_object:
+                    dm_key_dict['keys_data'].append(key)
+                else:
+                    dm_key_dict['keys_no_data'].append(key)
+            else:
+                dm_key_dict['keys_not_found'].append(key)
+        return dm_key_dict
+
+    @classmethod
+    def safeget(cls, dict, keys):
+        # Utility function to safely get nested dictionary values
+        for key in keys:
+            if dict is None:
+                return None
+            if key in dict:
+                dict = dict[key]
+            else:
+                return None
+
+        return dict
