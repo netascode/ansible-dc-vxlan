45538d2811963f8516396ac28ec42820ded85dc5
ccoueffe <ccoueffe@cisco.com>
2026-05-12 17:18:06 +0200

    refactor validate
    Signed-off-by: ccoueffe <ccoueffe@cisco.com>

--- plugins/action/common/merge_defaults.py
+++ plugins/action/common/merge_defaults.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024 Cisco Systems, Inc. and its affiliates
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
-# this software and associated documentation files (the "Software"), to deal in
+# this software and associated documentation files (the 'Software'), to deal in
 # the Software without restriction, including without limitation the rights to
 # use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
 # the Software, and to permit persons to whom the Software is furnished to do so,
@@ -10,7 +10,7 @@
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
 # FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
@@ -23,6 +23,9 @@ from __future__ import absolute_import, division, print_function
 
 
 __metaclass__ = type
+
+import os
+import yaml
 
 from ansible.utils.display import Display
 from ansible.plugins.action import ActionBase
@@ -40,23 +43,39 @@ def merge_dicts(d1, d2):
 
 
 class ActionModule(ActionBase):
-
     def run(self, tmp=None, task_vars=None):
-        # self._supports_async = True
         results = super(ActionModule, self).run(tmp, task_vars)
-        results['failed'] = False
-        results['msg'] = None
-        results['defaults'] = None
+        results["failed"] = False
+        results["msg"] = None
+        results["defaults"] = None
+        results["factory_defaults"] = None
 
-        # Get Data from Ansible Task
-        fac_def = self._task.args['factory_defaults']
-        data_model = self._task.args['data_model']
+        factory_defaults = self._task.args.get("factory_defaults")
+        factory_defaults_file = self._task.args.get("factory_defaults_file")
+        data_model = self._task.args["data_model"]
+
+        if factory_defaults is None and factory_defaults_file:
+            defaults_path = factory_defaults_file
+            if not os.path.isabs(defaults_path):
+                role_path = task_vars.get("role_path", "")
+                defaults_path = os.path.join(role_path, defaults_path)
+
+            if os.path.isfile(defaults_path):
+                with open(defaults_path, "r") as f:
+                    loaded = yaml.safe_load(f) or {}
+                factory_defaults = loaded.get("factory_defaults", loaded)
+            else:
+                factory_defaults = {}
+        elif factory_defaults is None:
+            factory_defaults = {}
+
+        results["factory_defaults"] = factory_defaults
 
         cus_def = {}
         if data_model is not None:
-            if data_model.get('defaults') is not None:
-                cus_def = data_model['defaults']
+            if data_model.get("defaults") is not None:
+                cus_def = data_model["defaults"]
 
-        results['defaults'] = merge_dicts(fac_def, cus_def)
+        results["defaults"] = merge_dicts(factory_defaults, cus_def)
 
         return results
